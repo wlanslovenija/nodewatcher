6ab94110f7c20be55a46551ee1c7df4921969eec
Jernej Kos <k@jst.sm>
2009-02-12 19:47:39 +0000

    Added a script for cleaning up stale image files.

---
+++ generator/genny-cleaner.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#
+# Wlan-Lj Image Generator Daemon
+#
+# Copyright (C) 2009 by Jernej Kos <kostko@unimatrix-one.org>
+#
+import os
+import time
+
+DESTINATION = "/var/www/nodes.wlan-lj.net/results"
+
+for root, dirs, files in os.walk(DESTINATION):
+  for file in files:
+    file = os.path.join(root, file)
+    if int(time.time() - os.path.getmtime(file)) > 86400 * 5:
+      os.unlink(file)
+
