1d8feea1ef7b3911f55399f88c1c9c45775f3b53
Jernej Kos <k@jst.sm>
2009-02-17 21:56:57 +0000

    Added VPN authentication script (with database support).

---
+++ nodewatcher/vpn/auth.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+# Setup import paths, since we are using Django models
+import sys, os
+sys.path.append('/var/www/django')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'wlanlj.settings'
+
+# Import our models
+from wlanlj.account.models import UserAccount
+from django.contrib.auth.models import User
+from django.db import transaction
+
+@transaction.commit_manually
+def authenticate_in_db(username, password):
+  """
+  Authenticates against the nodewatcher database.
+  """
+  try:
+    user = User.objects.get(username = username)
+    if user.get_profile().vpn_password == password:
+      return True
+  except User.DoesNotExist:
+    return False
+  except UserAccount.DoesNotExist:
+    return False
+
+f = open('/etc/openvpn/users.txt', 'r')
+lines = f.read().split('\n')
+f.close()
+
+username = os.getenv('username')
+password = os.getenv('password')
+
+for line in lines:
+  if not line:
+    continue
+
+  runame, rpass = line.split('\t')
+  if username == runame and password == rpass:
+    sys.exit(0)
+
+# Try the database now
+if authenticate_in_db(username, password):
+  sys.exit(0)
+
+sys.exit(1)
