d95bc132dc56f67823751119fd2328ad99a91614
Jernej Kos <k@jst.sm>
2009-03-01 14:27:31 +0000

    Don't forget iptables NAT packages.

--- generator/config_generator.py
+++ generator/config_generator.py
@@ -384,6 +384,7 @@ class OpenWrtConfig(NodeConfig):
 
     self.addPackage('ip', 'olsrd', 'ntpclient', 'wireless-tools', 'kmod-softdog', 'hotplug2', 'cronscripts')
     self.addPackage('kmod-ipt-conntrack', 'iptables-mod-conntrack')
+    self.addPackage('kmod-ipt-nat', 'iptables-mod-nat')
     self.addPackage('nodewatcher', 'olsrd-mod-actions')
 
     # Build the image
