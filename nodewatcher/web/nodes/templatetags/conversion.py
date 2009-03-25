c0d2c46b01635f1b7ef06361782321f822b2861f
Jernej Kos <k@jst.sm>
2009-03-25 12:31:25 +0000

    Added uptime display to node status page.

--- nodewatcher/web/nodes/templatetags/conversion.py
+++ nodewatcher/web/nodes/templatetags/conversion.py
@@ -13,4 +13,26 @@ def human_readable_kbytes(value):
   else:
     return "%.02f KB" % value
 
+def time_delta(value):
+  """
+  Returns properly formatted time delta string.
+  """
+  days = hours = minutes = 0
+
+  if value > 3600*24:
+    days = value // (3600*24)
+    value = value - days*3600*24
+
+  if value > 3600:
+    hours = value // 3600
+    value = value - hours*3600
+
+  if value > 60:
+    minutes = value // 60
+    value = value - minutes*60
+
+  return "%02d d %02d h %02d min %02d sec" % (days, hours, minutes, value)
+
 register.filter('human_readable_kbytes', human_readable_kbytes)
+register.filter('time_delta', time_delta)
+
