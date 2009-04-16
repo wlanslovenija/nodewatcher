805c9f5f63a05b1f9c2488343913f31f6235edcd
Mitar <mitar.git@tnode.com>
2009-04-16 14:04:56 +0000

    Changed notes display a little.

--- nodewatcher/web/nodes/forms.py
+++ nodewatcher/web/nodes/forms.py
@@ -22,7 +22,7 @@ class RegisterNodeForm(forms.Form):
   """
   name = forms.CharField(max_length = 50, label = _("Node name"))
   ip = forms.CharField(max_length = 40, required = False, label = _("IP address"))
-  location = forms.CharField(max_length = 200)
+  location = forms.CharField(max_length = 200, widget = widgets.TextInput(attrs = {'size': '40'}))
   geo_lat = forms.FloatField(required = False, label = _("Lattitude"))
   geo_long = forms.FloatField(required = False, label = _("Longitude"))
   notes = forms.CharField(max_length = 1000, required = False, label = _("Notes"), widget = widgets.Textarea)
