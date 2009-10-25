4d25672fb7e56d65ccb078af95847ddef31cf7c7
Jernej Kos <k@jst.sm>
2009-10-25 10:03:24 +0000

    Cleanups.

--- nodewatcher/web/nodes/templatetags/nodestatus.py
+++ nodewatcher/web/nodes/templatetags/nodestatus.py
@@ -4,47 +4,39 @@ from django.template import RequestContext
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
 
-
 register = Library()
 
 IMAGE_TEMPLATE = """<img src="/images/status_%(status)s_%(size)s.png" title="%(status)s" alt="%(status)s"/>"""
-
 WRAPPER_TEMPLATE = """<span class="node_status_%(status)s node_status_%(size)s">%(content)s</span>"""
 
-def statusimage(value, arg, autoescape=None):
+def statusimage(value, arg, autoescape = None):
+  value = value.lower()
+  
+  if not (value in ('up', 'down', 'invalid', 'visible', 'duped', 'pending', 'new')):
+    return ""
+  
+  if not (arg in ('big', 'small', 'gmap')):
+    arg = "small"
+  
+  params = {"status" : value, "size" : arg}
+  return mark_safe(IMAGE_TEMPLATE % params)
 
-    value = value.lower()
-
-    if not (value in ('up', 'down', 'invalid', 'visible', 'duped', 'pending', 'new')):
-      return ""
-
-    if not (arg in ('big', 'small', 'gmap')):
-      arg = "small"
-
-    params = {"status":value, "size":arg}
-
-    return mark_safe(IMAGE_TEMPLATE % params)
-
-def status(value, arg, autoescape=None):
-
-    value = value.lower()
-
-    if not (value in ('up', 'down', 'invalid', 'visible', 'duped', 'pending', 'new')):
-      return ""
-
-    if not (arg in ('big', 'small', 'gmap')):
-      arg = "small"
-
-    params = {"status":value, "size":arg}
-
-    params['content'] = (IMAGE_TEMPLATE % params) + "&nbsp;" + value
-
-    return mark_safe(WRAPPER_TEMPLATE % params)
+def status(value, arg, autoescape = None):
+  value = value.lower()
+  
+  if not (value in ('up', 'down', 'invalid', 'visible', 'duped', 'pending', 'new')):
+    return ""
+  
+  if not (arg in ('big', 'small', 'gmap')):
+    arg = "small"
+  
+  params = {"status" : value, "size" : arg}
+  params['content'] = (IMAGE_TEMPLATE % params) + "&nbsp;" + value
+  return mark_safe(WRAPPER_TEMPLATE % params)
 
 status.needs_autoescape = True
 statusimage.needs_autoescape = True
 
 register.filter('statusimage', statusimage)
-
 register.filter('status', status)
 
