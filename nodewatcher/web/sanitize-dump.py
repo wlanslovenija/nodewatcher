497c8ba5cab11ae842453efc03417ca18af9fd64
Jernej Kos <k@jst.sm>
2009-10-26 10:30:55 +0000

    Made dump sanitizer SQL-independent by using Django serializers.

--- nodewatcher/web/sanitize-dump.py
+++ nodewatcher/web/sanitize-dump.py
@@ -1,39 +1,42 @@
 #!/usr/bin/python
-import sys
-import os
-import MySQLdb
+
+# Setup import paths, since we are using Django models
+import sys, os
+sys.path.append('/var/www/django')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'wlanlj.settings_production'
+
+# Imports
+from django.core import serializers
 
 if len(sys.argv) != 4:
-  print "Usage: %s dump-file username password" % sys.argv[0]
+  print "Usage: %s format input-file output-file" % sys.argv[0]
   exit(1)
 
-input, username, password = sys.argv[1:]
+if sys.argv[1] not in ('json', 'xml'):
+  print "Invalid format '%s'! Valid formats are: json xml" % sys.argv[1]
+  exit(1)
 
-# Create database
-c = MySQLdb.connect(host = "localhost", user = username, passwd = password)
-cursor = c.cursor()
-cursor.execute("CREATE DATABASE __temp_wlanlj_sanitize")
+def object_transformator():
+  # Read all objects one by one
+  for holder in serializers.deserialize(sys.argv[1], open(sys.argv[2], 'r')):
+    name = holder.object.__class__.__name__
+    object = holder.object
+    
+    # Some objects need to be sanitized
+    if name == 'Node':
+      object.notes = ''
+    elif name == 'UserAccount':
+      object.vpn_password = 'XXX'
+      object.phone = '5551234'
+    elif name == 'User':
+      object.password = 'XXX'
+    elif name == 'Profile':
+      object.root_pass = 'XXX'
+    
+    yield holder.object
 
-# Import stuff
-os.system("sed -i 's/^CREATE DATABASE/--/g' %s" % input)
-os.system("sed -i 's/^USE/--/g' %s" % input)
-os.system("mysql -u %s --password=%s __temp_wlanlj_sanitize < %s" % (username, password, input))
-
-cursor.execute("UPDATE __temp_wlanlj_sanitize.account_useraccount SET vpn_password = 'XXX', phone = '5551234'")
-cursor.execute("UPDATE __temp_wlanlj_sanitize.auth_user SET password = ''")
-cursor.execute("UPDATE __temp_wlanlj_sanitize.generator_profile SET root_pass = 'XXX'")
-cursor.execute("DELETE FROM __temp_wlanlj_sanitize.django_session")
-cursor.close()
-c.commit()
-c.close()
-
-# Export stuff
-os.system("mysqldump -u %s --password=%s __temp_wlanlj_sanitize > DUMP-sanitized.sql" % (username, password))
-
-# Drop database
-c = MySQLdb.connect(host = "localhost", user = username, passwd = password)
-cursor = c.cursor()
-cursor.execute("DROP DATABASE __temp_wlanlj_sanitize")
-cursor.close()
-c.close()
+# Write transformed objects out
+out = open(sys.argv[3], 'w')
+serializers.serialize(sys.argv[1], object_transformator(), stream = out)
+out.close()
 
