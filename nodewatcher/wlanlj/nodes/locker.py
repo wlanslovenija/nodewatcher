b40c248dbbd624e4e065af27f777d60041d75a3f
Jernej Kos <k@jst.sm>
2009-10-30 11:24:34 +0000

    Added some information about locking practices.

--- nodewatcher/wlanlj/nodes/locker.py
+++ nodewatcher/wlanlj/nodes/locker.py
@@ -1,5 +1,20 @@
 from django.db import connection, transaction
 
+#
+# NOTE ABOUT LOCKING
+#
+# This function is needed to prevent concurrent access to
+# data in sections that need to be serializable.  There is
+# another option, namely to set transaction isolation level
+# via SET TRANSACTION ISOLATION LEVEL SERIALIZABLE, but this
+# has the drawback that it must be executed as the first
+# query in a transaction (and we have no control over this,
+# especially in Django).
+#
+# Therefore this method implements EXCLUSIVE table locks,
+# currently only on PostgreSQL.
+#
+
 def require_lock(*tables):
   def _lock(func):
     def _do_lock(*args,**kws):
