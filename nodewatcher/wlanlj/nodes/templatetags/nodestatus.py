506acae8fd88906f8957b1603f9529937cab30da
Mitar <mitar.git@tnode.com>
2009-12-07 16:56:38 +0000

    Some usability improvements.

--- nodewatcher/wlanlj/nodes/templatetags/nodestatus.py
+++ nodewatcher/wlanlj/nodes/templatetags/nodestatus.py
@@ -6,7 +6,18 @@ from django.utils.safestring import mark_safe
 
 register = Library()
 
-IMAGE_TEMPLATE = """<img src="/images/status_%(status)s_%(size)s.png" title="%(status)s" alt="%(status)s"/>"""
+DESCRIPTIONS = {
+  'up': 'node is reachable',
+  'visible': 'node is visible via OLSR but does not reply to ICMP ECHO',
+  'down': 'node is not visible via OLSR',
+  'duped': 'duplicate ICMP ECHO packets have been received',
+  'invalid': 'IP is not allocated but is seen',
+  'new': 'node has just been registered',
+  'pending': 'node has not yet been seen since registration',
+  'awaitingrenumber': 'node has been recently renumbered'
+}
+
+IMAGE_TEMPLATE = """<img src="/images/status_%(status)s_%(size)s.png" title="%(title)s" alt="%(status)s"/>"""
 WRAPPER_TEMPLATE = """<span class="node_status_%(status)s node_status_%(size)s">%(content)s</span>"""
 
 def statusimage(value, arg, autoescape = None):
@@ -18,7 +29,7 @@ def statusimage(value, arg, autoescape = None):
   if not (arg in ('big', 'small', 'gmap')):
     arg = "small"
   
-  params = {"status" : value, "size" : arg}
+  params = {"status" : value, "size" : arg, "title" : ("%s - %s" % (value, DESCRIPTIONS[value]))}
   return mark_safe(IMAGE_TEMPLATE % params)
 
 def status(value, arg, autoescape = None):
@@ -30,13 +41,16 @@ def status(value, arg, autoescape = None):
   if not (arg in ('big', 'small', 'gmap')):
     arg = "small"
   
-  params = {"status" : value, "size" : arg}
+  params = {"status" : value, "size" : arg, "title" : ("%s - %s" % (value, DESCRIPTIONS[value]))}
   params['content'] = (IMAGE_TEMPLATE % params) + "&nbsp;" + value
   return mark_safe(WRAPPER_TEMPLATE % params)
+
+@register.simple_tag
+def statusdesc(value):
+  return DESCRIPTIONS[value]
 
 status.needs_autoescape = True
 statusimage.needs_autoescape = True
 
 register.filter('statusimage', statusimage)
 register.filter('status', status)
-
