41342488a31ef06a231e4ea7d2c1d7467d10713d
Jernej Kos <k@jst.sm>
2010-04-28 08:28:30 +0000

    Made PostgreSQL database initialization script more flexible.

--- nodewatcher/wlanlj/scripts/postgresql_init.sh
+++ nodewatcher/wlanlj/scripts/postgresql_init.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -e
 
+CONTRIB_DIR=""
+if [ -d /usr/share/postgresql/8.4/contrib ]; then
+  CONTRIB_DIR="/usr/share/postgresql/8.4/contrib"
+elif [ -d /usr/share/postgresql-8.3/contrib ]; then
+  CONTRIB_DIR="/usr/share/postgresql-8.3/contrib"
+else
+  echo "!!! PostgreSQL contrib directory not found. Please edit"
+  echo "!!! the database initialization script!"
+  exit 1
+fi
+
 dropdb -U postgres wlanlj
 createdb -U postgres -E UNICODE -O wlanlj wlanlj
-psql -U postgres wlanlj -f /usr/share/postgresql-8.3/contrib/ip4r.sql
+psql -U postgres wlanlj -f ${CONTRIB_DIR}/ip4r.sql
+
