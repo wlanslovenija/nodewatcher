610ffadefe82ff92542f8f5ce661d2c50a78687a
Mitar <mitar.git@tnode.com>
2010-05-16 16:24:57 +0000

    Abstracting ''wlan ljubljana'' specific things. #275

--- nodewatcher/web/settings.py
+++ nodewatcher/web/settings.py
@@ -130,7 +130,7 @@ TEMPLATE_DIRS = (
   # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
   # Always use forward slashes, even on Windows.
   # Don't forget to use absolute paths, not relative paths.
-  wlanlj_template_dir,
+  wlanlj_template_dir, # Remove this entry and define you own directory with templates to override default ones
   default_template_dir,
 )
 
