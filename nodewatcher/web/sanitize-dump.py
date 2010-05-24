c2583e6978d1684b60a2b086b2cd753efc4dfe43
Mitar <mitar.git@tnode.com>
2010-05-24 18:58:37 +0000

    Sanitize dump a little bit more.

--- nodewatcher/web/sanitize-dump.py
+++ nodewatcher/web/sanitize-dump.py
@@ -21,23 +21,30 @@ if sys.argv[1] not in ('json', 'xml'):
 def object_transformator():
   # Read all objects one by one
   for holder in serializers.deserialize(sys.argv[1], open(sys.argv[2], 'r')):
-    name = holder.object.__class__.__name__
     object = holder.object
+    name = "%s.%s" % (object.__module__, object.__class__.__name__)
     
     # Some objects need to be sanitized
-    if name == 'Node':
+    if name == 'web.nodes.models.Node':
       object.notes = ''
-    elif name == 'UserAccount':
+    elif name == 'web.account.models.UserAccount':
       object.vpn_password = 'XXX'
+      object.name = ""
       object.phone = '5551234'
-    elif name == 'User':
+    elif name == 'django.contrib.auth.models.User':
       object.first_name = ""
       object.last_name = ""
       object.email = "user@example.com"
       object.password = '$1$1qL5F...$ZPQdHpHMsvNQGI4rIbAG70' # Password for all users is 123
-    elif name == 'Profile':
+    elif name == 'web.generator.models.Profile':
       object.root_pass = 'XXX'
-    elif name == 'StatsSolar':
+    elif name == 'web.generator.models.StatsSolar':
+      continue
+    elif name == 'web.generator.models.WhitelistItem':
+      continue
+    elif name == 'django.contrib.sessions.models.Session':
+      continue
+    elif name == 'django.contrib.auth.models.Message':
       continue
 
     yield holder.object
