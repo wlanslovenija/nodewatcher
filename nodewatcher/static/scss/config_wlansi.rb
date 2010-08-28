29fca4b804ae949ce4dc70e8dc9d6295f1e6e0a0
Luka Čehovin <luka.cehovin@gmail.com>
2010-08-28 19:28:45 +0000

    SCSS files for the generic and wlansi nodewatcher design

---
+++ nodewatcher/static/scss/config_wlansi.rb
@@ -0,0 +1,10 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "../wlansi/css"
+sass_dir = "wlansi"
+images_dir = "../wlansi/css/images"
+javascripts_dir = "../js"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
