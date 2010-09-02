7c1ea4e23d3121360be738f6e0f8191ca9395424
Jernej Kos <k@jst.sm>
2010-09-02 16:33:32 +0000

    Do not raise a warning when router-only nodes don't have a captive portal running. See ticket #431.

--- nodewatcher/monitor/monitor.py
+++ nodewatcher/monitor/monitor.py
@@ -495,7 +495,7 @@ def process_node(node_ip, ping_results, is_duped, peers, varsize_results):
           # Create a node warning when captive portal is down and the node has it
           # selected in its image generator profile
           try:
-            if n.project.captive_portal:
+            if n.project.captive_portal and n.has_client_subnet():
               NodeWarning.create(n, WarningCode.CaptivePortalDown, EventSource.Monitor)
           except Profile.DoesNotExist:
             pass
