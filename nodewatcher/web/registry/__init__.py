233f5d5cd1a23afcdbcc985e27d862344980be2a
Jernej Kos <k@jst.sm>
2011-04-25 00:54:24 +0200

    Removed unneeded import.

--- nodewatcher/web/registry/__init__.py
+++ nodewatcher/web/registry/__init__.py
@@ -1,8 +1,6 @@
 from django.core import exceptions
 from django.db import connection
 
-from registry import rules as registry_rules
-
 # Verify that we have full support for savepoints in the database backend
 if not connection.features.uses_savepoints:
   raise exceptions.ImproperlyConfigured("Nodewatcher requires a database backend that supports transactional savepoints! We recommend you to use PostgreSQL.")
