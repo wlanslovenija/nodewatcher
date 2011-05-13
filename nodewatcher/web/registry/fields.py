01ef7bb52c148914fd8d697e9a3ce01c1e9be432
Jernej Kos <k@jst.sm>
2011-05-13 20:59:12 +0200

    Added some missing documentation.

--- nodewatcher/web/registry/fields.py
+++ nodewatcher/web/registry/fields.py
@@ -10,6 +10,11 @@ from registry import registration
 from registry import models as registry_models
 
 class SelectorKeyField(models.CharField):
+  """
+  A character field that supports choices derived from a pre-registered choice set.
+  When the field is rendered inside the registry formset, any modifications to it
+  will cause the forms to be recomputed.
+  """
   def __init__(self, regpoint, enum_id, *args, **kwargs):
     """
     Class constructor.
@@ -19,6 +24,9 @@ class SelectorKeyField(models.CharField):
     super(SelectorKeyField, self).__init__(*args, **kwargs)
   
   def formfield(self, **kwargs):
+    """
+    Returns an augmented form field.
+    """
     defaults = { 'widget' : widgets.Select(attrs = { 'class' : 'regact_selector' }) }
     defaults.update(kwargs) 
     field = super(SelectorKeyField, self).formfield(**defaults)
@@ -35,6 +43,11 @@ class SelectorKeyField(models.CharField):
     return field
 
 class ModelSelectorKeyField(models.ForeignKey):
+  """
+  A standard foreign key that augments the resulting form widget with a special
+  selector class that will cause the forms to be recomputed when this field is
+  modified.
+  """
   def __init__(self, *args, **kwargs):
     """
     Class constructor.
@@ -43,17 +56,29 @@ class ModelSelectorKeyField(models.ForeignKey):
     super(ModelSelectorKeyField, self).__init__(*args, **kwargs)
   
   def formfield(self, **kwargs):
+    """
+    Returns an augmented form field.
+    """
     defaults = { 'widget' : widgets.Select(attrs = { 'class' : 'regact_selector' }) }
     defaults.update(kwargs) 
     return super(ModelSelectorKeyField, self).formfield(**defaults)
 
 class IntraRegistryRelatedObjectDescriptor(models.fields.related.ForeignRelatedObjectsDescriptor):
   """
+  Descriptor for accessing related objects of a intra-registry foreign key that
+  adds support for virtual relations in case of partially validated models.
   """
   def __init__(self, *args, **kwargs):
+    """
+    Class constructor.
+    """
     super(IntraRegistryRelatedObjectDescriptor, self).__init__(*args, **kwargs)
   
   def __get__(self, instance, instance_type = None):
+    """
+    Adds support for accessing virtual relations, since partially validated models
+    can't be saved and therefore references don't work.
+    """
     if not hasattr(instance, '_registry_virtual_model'):
       return super(IntraRegistryRelatedObjectDescriptor, self).__get__(instance, instance_type)
     elif instance is None:
@@ -62,16 +87,30 @@ class IntraRegistryRelatedObjectDescriptor(models.fields.related.ForeignRelatedO
       return getattr(instance, '_registry_virtual_relation', {}).get(self, [])
   
   def __set__(self, instance, value):
+    """
+    Adds support for setting virtual relations, since partially validated models
+    can't be saved and therefore references don't work.
+    """
     if not hasattr(instance, '_registry_virtual_model'):
       super(IntraRegistryRelatedObjectDescriptor, self).__set__(instance, value)
     else:
       instance._registry_virtual_relation[self] = value
 
 class IntraRegistryForeignKey(models.ForeignKey):
+  """
+  A field for connecting registry items into a hierarchy.
+  """
   def __init__(self, *args, **kwargs):
+    """
+    Class constructor.
+    """
     super(IntraRegistryForeignKey, self).__init__(*args, **kwargs)
   
   def contribute_to_related_class(self, cls, related):
+    """
+    Modifies the accessor descriptor so our own class is used instead of
+    the standard one.
+    """
     super(IntraRegistryForeignKey, self).contribute_to_related_class(cls, related)
     setattr(cls, related.get_accessor_name(), IntraRegistryRelatedObjectDescriptor(related))
 
