13941b2e9bf247f708a3865d853b5f8165fdce52
Mitar <mitar.git@tnode.com>
2011-10-16 17:05:17 +0200

    Added ''wlan slovenija'' background.

--- nodewatcher/static/scss/config/compass.rb
+++ nodewatcher/static/scss/config/compass.rb
@@ -5,5 +5,6 @@ css_dir = "../css"
 sass_dir = "default"
 images_dir = "../images"
 javascripts_dir = "../js"
+environment = :production
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
