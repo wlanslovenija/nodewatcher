c661ff2f63ee6942b0e40d9a047e8c07a86dfb7d
Mitar <mitar.git@tnode.com>
2011-10-16 15:03:06 +0200

    Updated compass configuration with new paths.

--- nodewatcher/static/scss/config.rb
+++ nodewatcher/static/scss/config.rb
@@ -1,10 +1,9 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "../wlansi/css"
-sass_dir = "wlansi"
-images_dir = "../wlansi/css/images"
+css_dir = "../css"
+sass_dir = "default"
+images_dir = "../images"
 javascripts_dir = "../js"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
-
