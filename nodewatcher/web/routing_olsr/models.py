3512e6bc04f76bb504a2aededb2b73b78b6d2ffe
Jernej Kos <k@jst.sm>
2012-01-16 15:15:21 +0100

    Removed unneeded import.

--- nodewatcher/web/routing_olsr/models.py
+++ nodewatcher/web/routing_olsr/models.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.db import models, transaction
+from django.db import models
 from django.utils.translation import ugettext as _
 
 from web.core import models as core_models
