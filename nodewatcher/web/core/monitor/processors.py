c49db708a459528d6ea9dbff18e755e017335995
Jernej Kos <k@jst.sm>
2012-04-09 16:45:21 +0200

    Fixed handling of namespaces in ProcessorContext and added a useful depends_on_context decorator.

--- nodewatcher/web/core/monitor/processors.py
+++ nodewatcher/web/core/monitor/processors.py
@@ -19,27 +19,69 @@ class ProcessorContext(dict):
   
   def __getitem__(self, key):
     """
-    Dictionary access.
+    Namespace-aware __getitem__.
+    """
+    if self._namespace:
+      return self._ns().__getitem__(key)
+    return self._get(key)
+
+  def __setitem__(self, key, value):
+    """
+    Namespace-aware __setitem__.
+    """
+    if self._namespace:
+      return self._ns().__setitem__(key, value)
+    super(ProcessorContext, self).__setitem__(key, value)
+
+  def _get(self, key):
+    """
+    Get that automatically creates ProcessorContexts when key doesn't exist.
     """
     try:
-      return dict.__getitem__(self, key)
+      return super(ProcessorContext, self).__getitem__(key)
     except KeyError:
       if key.startswith('_'):
         raise
-      
-      return dict.setdefault(self, key, ProcessorContext())
-  
+
+      return super(ProcessorContext, self).setdefault(key, ProcessorContext())
+
+  def __contains__(self, item):
+    """
+    Namespace-aware __contains__.
+    """
+    if self._namespace:
+      return self._ns().__contains__(item)
+    return super(ProcessorContext, self).__contains__(item)
+
+  def _ns(self):
+    """
+    Namespace resolution.
+    """
+    return self._namespace[-1] if self._namespace else self
+
   def __getattr__(self, name):
     """
     Attribute access.
     """
-    return reduce(lambda x, y: x[y], self._namespace, self)[name]
+    if name.startswith('_'):
+      return super(ProcessorContext, self).__getattr__(name)
+    return self[name]
   
   def __setattr__(self, name, value):
     """
     Attribute update.
     """
-    reduce(lambda x, y: x[y], self._namespace, self)[name] = value
+    if name.startswith('_'):
+      return super(ProcessorContext, self).__setattr__(name, value)
+    self[name] = value
+
+  def setdefault(self, k, d=None):
+    """
+    Namespace-aware setdefault.
+    """
+    if self._namespace:
+      return self._ns().setdefault(k, d)
+    return super(ProcessorContext, self).setdefault(k, d)
 
   def merge_with(self, other):
     """
@@ -57,13 +99,20 @@ class ProcessorContext(dict):
     return self
   
   @contextlib.contextmanager
-  def in_namespace(self, namespace):
+  def in_namespace(self, namespace, ctx_class = None):
     """
     Starts a new namespace.
     
     @param namespace: Namespace name
+    @param ctx_class: Optional context class to use for this namespace root (should
+      be a subclass of `ProcessorContext`)
     """
-    self._namespace.append(namespace)
+    # Create a new instance
+    ns = ctx_class() if ctx_class else ProcessorContext()
+    self[namespace] = ns
+
+    # Append to list of namespaces and enter the context
+    self._namespace.append(ns)
     yield
     self._namespace.pop()
 
@@ -140,3 +189,20 @@ def register_processor(processor):
   global processors
   processors.append(processor())
 
+def depends_on_context(*keys):
+  """
+  A decorator that augments methods which receive context as a first
+  argument and return a context. It ensures that the method is only called
+  when certain keys are present in the method's context.
+  """
+  def decorator(f):
+    def wrapper(self, context, *args, **kwargs):
+      for key in keys:
+        if key not in context:
+          return context
+
+      f(self, context, *args, **kwargs)
+
+    return wrapper
+
+  return decorator
