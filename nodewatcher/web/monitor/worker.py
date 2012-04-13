cd21d5ee3760b40e53d7eb539dbca6202c9c433f
Jernej Kos <k@jst.sm>
2012-04-13 22:47:42 +0200

    Avoid overlong transaction blocks.

--- nodewatcher/web/monitor/worker.py
+++ nodewatcher/web/monitor/worker.py
@@ -19,7 +19,7 @@ BANNER = """
 # Logger instance
 logger = logging.getLogger("monitor.worker")
 
-@transaction.commit_on_success
+@transaction.commit_manually
 def stage_worker(args):
   """
   Runs stage processors on a given node.
@@ -27,18 +27,18 @@ def stage_worker(args):
   context, node, stage = args
   for p in monitor_processors.processors:
     try:
-      sid = transaction.savepoint()
       getattr(p, stage)(context, node)
-      transaction.savepoint_commit(sid)
+      transaction.commit()
     except KeyboardInterrupt:
+      transaction.rollback()
       raise
     except:
-      transaction.savepoint_rollback(sid)
+      transaction.rollback()
       logger.error("First stage processor has failed with exception:")
       logger.error(traceback.format_exc())
 
 class Worker(object):
-  @transaction.commit_on_success
+  @transaction.commit_manually
   def cycle(self):
     """
     Performs a single monitoring cycle.
@@ -50,23 +50,24 @@ class Worker(object):
     for p in monitor_processors.processors:
       try:
         logger.info("Running preprocessor %s..." % p.__class__.__name__)
-        sid = transaction.savepoint()
         context, nodes = p.preprocess(context, nodes)
-        transaction.savepoint_commit(sid)
+        transaction.commit()
       except KeyboardInterrupt:
+        transaction.rollback()
         raise
       except:
-        transaction.savepoint_rollback(sid)
+        transaction.rollback()
         logger.error("Preprocessor has failed with exception:")
         logger.error(traceback.format_exc())
 
     # Find nodes that haven't been claimed by any processor and are invalid; such
     # nodes should be removed from the database
     logger.info("Purging unclaimed stale nodes...")
-    for node in nodes_models.Node.objects.regpoint("monitoring") \
-      .filter(statusmonitor_status = "invalid") \
-      .exclude(pk__in = nodes):
-      node.delete()
+    with transaction.commit_on_success():
+      for node in nodes_models.Node.objects.regpoint("monitoring") \
+        .filter(statusmonitor_status = "invalid") \
+        .exclude(pk__in = nodes):
+        node.delete()
 
     # Perform per-node processing in two stages
     logger.info("Running per-node first pass processors...")
