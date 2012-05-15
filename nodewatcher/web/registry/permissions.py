e42f0dbb2a06671c64e6d8d02b7962c1d2385536
Jernej Kos <k@jst.sm>
2012-05-15 14:27:19 +0200

    Prevent double registration of permissions.

--- nodewatcher/web/registry/permissions.py
+++ nodewatcher/web/registry/permissions.py
@@ -3,5 +3,10 @@ def register(model, permission, description):
   """
   Dynamically adds a new permission into an existing model.
   """
+  # Prevent double registration of permissions
+  for existing_perm, _ in model._meta.permissions:
+    if existing_perm == permission:
+      return
+
   model._meta.permissions.append((permission, description))
 
