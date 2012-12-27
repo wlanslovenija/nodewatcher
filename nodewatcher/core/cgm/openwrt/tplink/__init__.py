0bfaf1fe834da9c1f1084cfed610cc1177fd32c8
Jernej Kos <k@jst.sm>
2012-12-27 16:31:13 +0100

    Added some stub router descriptors to support latest dumps.

--- nodewatcher/core/cgm/openwrt/tplink/__init__.py
+++ nodewatcher/core/cgm/openwrt/tplink/__init__.py
@@ -1,2 +1,7 @@
 # TP-Link router modules
+import nodewatcher.core.cgm.openwrt.tplink.wr703n
+import nodewatcher.core.cgm.openwrt.tplink.wr740nd
 import nodewatcher.core.cgm.openwrt.tplink.wr741nd
+import nodewatcher.core.cgm.openwrt.tplink.wr841nd
+import nodewatcher.core.cgm.openwrt.tplink.wr842nd
+import nodewatcher.core.cgm.openwrt.tplink.mr3020
---
+++ nodewatcher/core/cgm/openwrt/tplink/mr3020.py
@@ -0,0 +1,45 @@
+from nodewatcher.registry.cgm import base as cgm_base
+from nodewatcher.registry.cgm import routers as cgm_routers
+from nodewatcher.registry.cgm import protocols as cgm_protocols
+
+class TPLinkMR3020(cgm_routers.RouterBase):
+    """
+    TP-Link MR3020 device descriptor.
+    """
+    identifier = "mr3020"
+    name = "MR3020"
+    manufacturer = "TP-Link"
+    url = "http://www.tp-link.com/"
+    architecture = "ar71xx"
+    radios = [
+      cgm_routers.IntegratedRadio("wifi0", "Wifi0", [cgm_protocols.IEEE80211BG, cgm_protocols.IEEE80211N], [
+        cgm_routers.AntennaConnector("a1", "Antenna0")
+      ])
+    ]
+    ports = [
+      cgm_routers.EthernetPort("wan0", "Wan0"),
+      cgm_routers.EthernetPort("lan0", "Lan0")
+    ]
+    antennas = [
+      # TODO this information is probably not correct
+      cgm_routers.InternalAntenna(
+        identifier = "a1",
+        polarization = "horizontal",
+        angle_horizontal = 360,
+        angle_vertical = 75,
+        gain = 2
+      )
+    ]
+    features = [
+      cgm_routers.Features.MultipleSSID,
+    ]
+    port_map = {
+      "openwrt": {
+        "wifi0" : "radio0",
+        "wan0"  : "eth1",
+        "lan0"  : "eth0",
+      }
+    }
+
+# Register the TP-Link MR3020 router
+cgm_base.register_router("openwrt", TPLinkMR3020)
