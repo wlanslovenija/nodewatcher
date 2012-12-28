4e5c995183c058e895384d02b9862c135a094af6
Mitar <mitar.git@tnode.com>
2012-12-28 15:50:15 +0100

    Code style.

--- nodewatcher/core/registry/rules/scope.py
+++ nodewatcher/core/registry/rules/scope.py
@@ -1,6 +1,5 @@
 from nodewatcher.core.registry.rules import engine
 from nodewatcher.core.registry.rules.predicates import *
 
-# Inject the engine context into the rule scope so it will be found by rule
-# predicates
+# Inject the engine context into the rule scope so it will be found by rule predicates
 ctx = engine.EngineContext()
--- nodewatcher/urls.py
+++ nodewatcher/urls.py
@@ -2,11 +2,11 @@ from django.conf import urls
 
 urlpatterns = urls.patterns('',
     # Registry
-    urls.url(r'^registry/', urls.include('nodewatcher.core.registry.urls', namespace = 'registry')),
+    urls.url(r'^registry/', urls.include('nodewatcher.core.registry.urls', namespace='registry')),
 
     # Accounts
     urls.url(r'account/', urls.include('nodewatcher.extra.account.urls')),
-    urls.url(r'^user/(?P<username>[\w.@+-]+)/$', 'nodewatcher.extra.account.views.user', name = 'user_page'),
+    urls.url(r'^user/(?P<username>[\w.@+-]+)/$', 'nodewatcher.extra.account.views.user', name='user_page'),
 
     # Frontend
     urls.url(r'^', urls.include('nodewatcher.core.frontend.urls')),
