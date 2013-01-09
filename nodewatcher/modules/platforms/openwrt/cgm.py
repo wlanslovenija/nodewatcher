5c06a2734a5114f8952dc5b86dc24327a93ccdbe
Jernej Kos <k@jst.sm>
2013-01-09 11:19:33 +0100

    Added wireless protocol configuration for OpenWRT.

--- nodewatcher/modules/platforms/openwrt/cgm.py
+++ nodewatcher/modules/platforms/openwrt/cgm.py
@@ -329,7 +329,18 @@ def network(node, cfg):
             dsc_radio = router.get_radio(interface.wifi_radio)
             dsc_protocol = dsc_radio.get_protocol(interface.protocol)
             dsc_channel = dsc_protocol.get_channel(interface.channel)
-            # TODO protocol details
+            if dsc_protocol.identifier == "ieee-80211bg":
+                radio.hwmode = '11g'
+            elif dsc_protocol.identifier == "ieee-80211n":
+                radio.hwmode = '11ng'
+                radio.htmode = 'HT20' # TODO: Should band width be made configurable?
+                radio.ht_capab = []
+
+                for capability in dsc_protocol.available_capabilities:
+                    radio.ht_capab.append(capability.identifier)
+            else:
+                raise cgm_base.ValidationError(_("Unsupported OpenWRT wireless protocol '%s'!") % dsc_protocol.identifier)
+
             radio.phy = "phy%d" % dsc_radio.index
             radio.channel = dsc_channel.number
 
