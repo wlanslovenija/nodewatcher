f7697f4bb8d6bb8aa4626586b3ddfe58cdcb21fa
Mitar <mitar.git@tnode.com>
2013-07-05 12:12:42 -0700

    Added comment.

--- nodewatcher/core/frontend/management/commands/runserver.py
+++ nodewatcher/core/frontend/management/commands/runserver.py
@@ -1,1 +1,6 @@
+# We override runserver command with basic runserver command which does not
+# dynamically serve static files so that post-processing SCSS files work
+# Static files have to be collected manually before running this command
+# TODO: Check if there is some way to make it work with staticfiles' runserver
+# TODO: Check if there is a way that it would automatically regenerate files on any file change during development
 from django.core.management.commands.runserver import Command
