8b6dccc51409b9d33eafdd7233579172ece72421
Jernej Kos <k@jst.sm>
2013-08-28 11:46:05 +0200

    Added some router profiles.

--- generator/config_generator.py
+++ generator/config_generator.py
@@ -65,6 +65,8 @@ portLayouts = {
 
   'tp-wr703n' : None,
   'tp-mr3020' : None,
+  'tp-mr3040' : None,
+  'tp-wr1043' : None,
   'ub-bullet' : None,
   'ub-nano'   : None,
   'ub-bullet-m5' : None,
@@ -1004,6 +1006,8 @@ config uhttpd main
         "tp-wr1043nd" : "TLWR1043",
         "tp-wr703n" : "TLWR703",
         "tp-mr3020" : "TLMR3020",
+        "tp-mr3040" : "TLMR3040",
+        "tp-wr1043" : "TLWR1043",
         
         "ub-bullet" : "UBNT",
         "ub-nano" : "UBNT",
