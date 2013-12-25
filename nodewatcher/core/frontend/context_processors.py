098f7996994db200b492d8cf4b88529508c2d1ef
Mitar <mitar.git@tnode.com>
2013-12-25 19:40:09 +0100

    Support True, False and None values in templates for Django < 1.5.

--- nodewatcher/core/frontend/context_processors.py
+++ nodewatcher/core/frontend/context_processors.py
@@ -1,3 +1,4 @@
+import django
 from django.conf import settings
 from django.contrib import auth
 from django.contrib.sites import models as sites_models
@@ -8,10 +9,21 @@ def global_vars(request):
     Adds some global variables to the context.
     """
 
-    return {
+    vars = {
         'NETWORK': getattr(settings, 'NETWORK', {}),
         'REDIRECT_FIELD_NAME': auth.REDIRECT_FIELD_NAME,
 
         'site': sites_models.get_current_site(request),
         'request_get_next': request.REQUEST.get(auth.REDIRECT_FIELD_NAME, ''),
     }
+
+    if django.VERSION < (1, 5):
+        # In Django versions before 1.5, you could not use True, False or None
+        # values directly in the template
+        vars.update({
+            'True': True,
+            'False': False,
+            'None': None,
+        })
+
+    return vars
