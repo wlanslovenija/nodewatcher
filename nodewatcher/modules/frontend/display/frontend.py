ddbbe9e5d64b4ba9f03ef6c9f06a7c540f33d171
Mitar <mitar.git@tnode.com>
2014-07-23 00:25:07 +0200

    Snippet and maintainers for node display.

--- nodewatcher/modules/frontend/display/frontend.py
+++ nodewatcher/modules/frontend/display/frontend.py
@@ -1,6 +1,8 @@
 from django.conf import urls
 from django.utils.translation import ugettext_lazy as _
 
+from guardian import shortcuts
+
 from nodewatcher.core.frontend import components
 from nodewatcher.core.registry import exceptions
 
@@ -35,15 +37,35 @@ components.partials.get_partial('node_snippet_partial').add(components.PartialEn
 def node_last_seen(context):
     try:
         last_seen = context['node'].monitoring.core.general().last_seen
-    except (exceptions.RegistryItemNotRegistered, AttributeError):
+    except exceptions.RegistryItemNotRegistered:
+        # If module is not loaded we want it to be false
         last_seen = None
+    except AttributeError:
+        # If attribute does not exist (module is loaded by no monitoring data is available), we display "unknown"
+        last_seen = _("unknown")
 
     return {
-        'node_last_seen': last_seen or _("unknown")
+        'node_last_seen': last_seen
     }
 
 components.partials.get_partial('node_snippet_partial').add(components.PartialEntry(
     name='last_seen',
     template='nodes/snippet/last_seen.html',
     extra_context=node_last_seen,
+))
+
+
+components.partials.register(components.Partial('node_display_partial'))
+
+components.partials.get_partial('node_display_partial').add(components.PartialEntry(
+    name='snippet',
+    template='nodes/display/snippet.html',
+))
+
+components.partials.get_partial('node_display_partial').add(components.PartialEntry(
+    name='maintainers',
+    template='nodes/display/maintainers.html',
+    extra_context=lambda context: {
+        'maintainers': shortcuts.get_users_with_perms(context['node']),
+    },
 ))
