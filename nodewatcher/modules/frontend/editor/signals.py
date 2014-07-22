8a24a32d9ff043873f866533c43229585d174038
Mitar <mitar.git@tnode.com>
2014-07-22 13:08:29 +0200

    Improved signal comment.

--- nodewatcher/modules/frontend/editor/signals.py
+++ nodewatcher/modules/frontend/editor/signals.py
@@ -4,5 +4,6 @@ from django import dispatch
 # Called when node is reset.
 reset_node = dispatch.Signal(providing_args=['request', 'node'])
 
-# Called when node is removed.
+# Called when node is removed. This is just when user interface triggers removal. Use
+# django.db.models.signals.post_delete on Node model if you want to hook into object removal.
 remove_node = dispatch.Signal(providing_args=['request', 'node'])
