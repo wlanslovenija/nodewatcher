21e03e40b7b33ec7e5b0219a48c869c74456bd23
Jernej Kos <jernej@kos.mx>
2014-07-22 13:34:11 +0200

    Nicer signal handling.

--- nodewatcher/modules/monitor/datastream/models.py
+++ nodewatcher/modules/monitor/datastream/models.py
@@ -1,3 +1,4 @@
+from django import dispatch
 from django.db.models import signals as django_signals
 from django.utils.translation import gettext_noop
 
@@ -534,27 +535,25 @@ class WifiInterfaceMonitorStreams(InterfaceMonitorStreams):
 pool.register(models.WifiInterfaceMonitor, WifiInterfaceMonitorStreams)
 
 
-def datastream_node_removed(sender, instance=None, **kwargs):
+@dispatch.receiver(django_signals.post_delete, sender=core_models.Node)
+def datastream_node_removed(sender, instance, **kwargs):
     """
     Remove all streams when a node gets removed.
     """
 
     datastream.delete_streams({'node': instance.pk})
 
-django_signals.post_delete.connect(datastream_node_removed, sender=core_models.Node)
-
 # In case we have the frontend module installed, we also subscribe to its
 # reset signal that gets called when a user requests a node's data to be reset
 try:
     from nodewatcher.modules.frontend.editor import signals as editor_signals
 
-    def datastream_node_reset(sender, request=None, node=None, **kwargs):
+    @dispatch.receiver(editor_signals.reset_node)
+    def datastream_node_reset(sender, request, node, **kwargs):
         """
         Remove all streams when a user requests the node to be reset.
         """
 
         datastream.delete_streams({'node': node.pk})
-
-    editor_signals.reset_node.connect(datastream_node_reset)
 except ImportError:
     pass
