dbc80dd22b5073f475cf71471b2885ca0885a3e8
Jernej Kos <jernej@kos.mx>
2014-10-17 12:00:11 +0200

    Changed URL for "my builds".

--- nodewatcher/modules/frontend/generator/frontend.py
+++ nodewatcher/modules/frontend/generator/frontend.py
@@ -14,8 +14,8 @@ class GeneratorComponent(components.FrontendComponent):
 
             urls.url(r'^node/(?P<pk>[^/]+)/generate_firmware/$', views.GenerateFirmware.as_view(), name='generate_firmware'),
 
-            urls.url(r'^generator/builds$', views.ListBuilds.as_view(), name='list_builds'),
-            urls.url(r'^generator/build/(?P<pk>[^/]+)$', views.ViewBuild.as_view(), name='view_build'),
+            urls.url(r'^my/builds$', views.ListBuilds.as_view(), name='list_builds'),
+            urls.url(r'^my/builds/(?P<pk>[^/]+)$', views.ViewBuild.as_view(), name='view_build'),
         )
 
 components.pool.register(GeneratorComponent)
