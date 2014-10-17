da4948b9ca98d26a1f8b990fd4154437e6f79122
Jernej Kos <jernej@kos.mx>
2014-10-17 09:35:58 +0200

    Modernized the module loader.

--- nodewatcher/utils/loader.py
+++ nodewatcher/utils/loader.py
@@ -1,4 +1,4 @@
-from django.conf import settings
+from django.apps import apps
 from django.utils import importlib, module_loading
 
 
@@ -8,13 +8,14 @@ def load_modules(*types):
     before registry operations can function normally.
 
     :param types: Types of modules that should be loaded (type name
-                  determines the filename that is loaded)
+      determines the filename that is loaded)
     """
 
-    for app in settings.INSTALLED_APPS:
-        mod = importlib.import_module(app)
-
+    # Note that we can't simply use module_loading.autodiscover_modules as it
+    # doesn't work correctly when multiple module types are specified (it exits
+    # on the first import failure). See: https://code.djangoproject.com/ticket/23670
+    for app in apps.get_app_configs():
         for type in types:
             # Attempt to import the submodule if it exists
-            if module_loading.module_has_submodule(mod, type):
-                importlib.import_module(".%s" % type, app)
+            if module_loading.module_has_submodule(app.module, type):
+                importlib.import_module(".%s" % type, app.name)
