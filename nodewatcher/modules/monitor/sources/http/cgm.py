a48e04f00dbbe692b2b8d94bfd8445031767d618
Jernej Kos <jernej@kos.mx>
2014-11-17 19:12:14 +0100

    Include uhttpd package for monitoring. Closes #1248.

--- nodewatcher/modules/monitor/sources/http/cgm.py
+++ nodewatcher/modules/monitor/sources/http/cgm.py
@@ -1,3 +1,6 @@
+from django.utils.translation import ugettext as _
+
+from nodewatcher.core import models as core_models
 from nodewatcher.core.generator.cgm import base as cgm_base
 
 
@@ -7,7 +10,27 @@ def general(node, cfg):
     Register packages needed for reporting via HTTP.
     """
 
+    # Configure the uhttpd server
+    uhttpd = cfg.uhttpd.add(uhttpd='main')
+    uhttpd.listen_http = []
+    uhttpd.home = '/www'
+    uhttpd.cgi_prefix = '/cgi-bin'
+
+    uhttpd.script_timeout = 60
+    uhttpd.network_timeout = 30
+    uhttpd.tcp_keepalive = 1
+    uhttpd.no_dirlists = 1
+
+    try:
+        router_id = node.config.core.routerid(queryset=True).get(rid_family='ipv4').router_id
+        uhttpd.listen_http.append('%s:80' % router_id)
+    except core_models.RouterIdConfig.DoesNotExist:
+        raise cgm_base.ValidationError(
+            _("In order to use nodewatcher monitoring, the node must have a configured primary IP address.")
+        )
+
     cfg.packages.update([
+        'uhttpd',
         'nodewatcher-agent',
         'nodewatcher-agent-mod-general',
     ])
