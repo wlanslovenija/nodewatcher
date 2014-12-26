1a520a7c0ecf16b02b74abaa5c211069d5b7b6aa
Mitar <mitar.git@tnode.com>
2014-12-26 21:43:36 +0100

    First test.

--- nodewatcher/modules/frontend/list/tests.py
+++ nodewatcher/modules/frontend/list/tests.py
@@ -1,8 +1,9 @@
 import datetime
-import uuid
+import json
 
 from django.core import urlresolvers
 from django.contrib.auth import models as auth_models
+from django.utils import encoding, timezone
 
 from tastypie import test
 
@@ -19,6 +20,8 @@ from . import resources
 
 class NodeResourceTest(test.ResourceTestCase):
     api_name = 'v1'
+    # To display full diff always.
+    maxDiff = None
 
     def setUp(self):
         super(NodeResourceTest, self).setUp()
@@ -47,10 +50,11 @@ class NodeResourceTest(test.ResourceTestCase):
         self.monitoring_monitored = [True, False, None]
         self.monitoring_health = list(status_models.StatusMonitor._meta.get_field('health').get_registered_choices())
 
-        self.initial_time = datetime.datetime(2014, 11, 5, 1, 5, 0)
+        self.initial_time = datetime.datetime(2014, 11, 5, 1, 5, 0, tzinfo=timezone.utc)
 
+        self.nodes = []
         for i in range(45):
-            node = core_models.Node(uuid=uuid.uuid4())
+            node = core_models.Node()
             node.save()
 
             node.config.core.general(
@@ -68,7 +72,7 @@ class NodeResourceTest(test.ResourceTestCase):
             node.monitoring.core.general(
                 create=monitor_models.GeneralMonitor,
                 first_seen=self.initial_time,
-                last_seen=self.initial_time + datetime.timedelta(seconds=i * 97),
+                last_seen=self.initial_time + datetime.timedelta(seconds=i),
             )
 
             # Status
@@ -104,12 +108,62 @@ class NodeResourceTest(test.ResourceTestCase):
                 geolocation='POINT(%f %f)' % (10 + (i / 100.0), 40 + (i / 100.0)),
             )
 
+            self.nodes.append(node)
+
     def resource_list_uri(self, resource_name):
         return urlresolvers.reverse('api:api_dispatch_list', kwargs={'api_name': self.api_name, 'resource_name': resource_name})
 
     def get_list(self, **kwargs):
+        kwargs['format'] = 'json'
+
         response = self.api_client.get(self.node_list, data=kwargs)
 
         self.assertValidJSONResponse(response)
 
         return self.deserialize(response)
+
+    def test_get_list_all(self):
+        data = self.get_list(
+            offset=0,
+            limit=0,
+        )
+
+        nodes = data['objects']
+        self.assertEqual(len(nodes), len(self.nodes))
+
+        for i, json_node in enumerate(nodes):
+            node = self.nodes[i]
+
+            self.assertEqual(json_node, {
+                u'status': {
+                    u'health': node.monitoring.core.status().health,
+                    u'monitored': node.monitoring.core.status().monitored,
+                    u'network': node.monitoring.core.status().network,
+                },
+                u'uuid': encoding.force_unicode(node.uuid),
+                u'name': node.config.core.general().name,
+                u'project': node.config.core.project().project.name,
+                u'location':  {
+                    u'address': u'Location %s' % i,
+                    u'altitude': 0.0,
+                    u'city': u'Ljubljana',
+                    u'country': u'SI',
+                    u'geolocation': json.loads(node.config.core.location().geolocation.geojson),
+                    u'timezone': u'Europe/Ljubljana',
+                },
+                # We manually construct URI to make sure it is like we assume it is.
+                u'resource_uri': u'%s%s/' % (self.node_list, node.uuid),
+                u'type': node.config.core.type().type,
+                # Works correctly only if i < 60.
+                u'last_seen': u'Wed, 05 Nov 2014 01:05:%02d +0000' % i,
+            })
+
+        self.assertEqual(data['meta'], {
+            u'total_count': 45,
+            # We specified 0 for limit in the request, so max limit should be used.
+            u'limit': resources.NodeResource.Meta.max_limit,
+            u'offset': 0,
+            u'nonfiltered_count': 45,
+            u'next': None,
+            u'previous': None,
+        })
