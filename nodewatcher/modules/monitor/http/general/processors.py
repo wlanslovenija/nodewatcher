c1137d12a7f0d4f885fcd2e74c9ce142de6a3da3
Jernej Kos <jernej@kos.mx>
2015-03-28 09:38:18 +0100

    Do not reset monitored general and status attributes.

--- nodewatcher/modules/monitor/http/general/processors.py
+++ nodewatcher/modules/monitor/http/general/processors.py
@@ -22,28 +22,17 @@ class GeneralInfo(monitor_processors.NodeProcessor):
         :return: A (possibly) modified context
         """
 
-        general = node.monitoring.core.general()
-        if general is not None:
-            general.uuid = None
-            general.firmware = None
-
-        status = node.monitoring.system.status()
-        if status is not None:
-            status.uptime = None
-            status.local_time = None
-
         version = context.http.get_module_version("core.general")
         if version == 0:
-            # Unsupported version or data fetch failed (v0)
-            if general:
-                general.save()
-            if status:
-                status.save()
+            # Unsupported version or data fetch failed (v0).
             return context
 
-        # Create models when they don't yet exist
+        # Create models when they don't yet exist.
+        general = node.monitoring.core.general()
         if general is None:
             general = node.monitoring.core.general(create=monitor_models.GeneralMonitor)
+
+        status = node.monitoring.system.status()
         if status is None:
             status = node.monitoring.system.status(create=monitor_models.SystemStatusMonitor)
 
@@ -55,7 +44,7 @@ class GeneralInfo(monitor_processors.NodeProcessor):
             general.uuid = context.http.core.general.uuid
             general.firmware = context.http.core.general.version
 
-            # Process node uptime and local time (v4+)
+            # Process node uptime and local time (v4+).
             status.uptime = int(context.http.core.general.uptime)
             local_time = int(context.http.core.general.local_time)
             status.local_time = datetime.datetime.fromtimestamp(local_time, pytz.utc)
@@ -63,7 +52,7 @@ class GeneralInfo(monitor_processors.NodeProcessor):
             general.uuid = context.http.general.uuid
             general.firmware = context.http.general.version
 
-            # Process node uptime and local time (v1+)
+            # Process node uptime and local time (v1+).
             uptime, _ = context.http.general.uptime.split()
             status.uptime = int(float(uptime))
             local_time = int(context.http.general.local_time)
