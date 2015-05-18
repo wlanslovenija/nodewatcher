cb51515cb61c4a5ba36072b69706aede3ce9788d
Jernej Kos <jernej@kos.mx>
2015-05-18 16:14:18 +0200

    Include more data in banner files.
    Fixes #1105.

--- nodewatcher/modules/administration/banner/cgm.py
+++ nodewatcher/modules/administration/banner/cgm.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+
 from nodewatcher.core.generator.cgm import base as cgm_base
 
 
@@ -15,3 +17,17 @@ def banner(node, cfg):
  +-------------------------------------------------+
 
 """
+
+    # Obtain the device descriptor so we can include additional stuff into
+    # the login banner by default.
+    general = node.config.core.general()
+    device = general.get_device()
+    cfg.banner += "        Node: %s\n" % general.name
+    cfg.banner += "        UUID: %s\n" % node.uuid
+    cfg.banner += "      Device: %s %s\n" % (device.manufacturer, device.name)
+    cfg.banner += "  Configured: %s\n" % (timezone.now().strftime('%d.%m.%Y %H:%M:%S %Z'))
+    cfg.banner += "\n"
+
+    # TODO: We currently cannot include the firmware version as configuration is generated before building.
+
+    # TODO: Datetime should probably be formatted based on the node's local timezone.
