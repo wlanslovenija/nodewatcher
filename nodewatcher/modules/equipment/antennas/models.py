fa8f5f018b7bee8e4c2f85e955879e1e032ea390
Jernej Kos <jernej@kos.mx>
2015-07-17 12:27:38 +0200

    Antenna field may be null.

--- nodewatcher/modules/equipment/antennas/models.py
+++ nodewatcher/modules/equipment/antennas/models.py
@@ -46,7 +46,7 @@ class AntennaEquipmentConfig(registration.bases.NodeConfigRegistryItem):
     device = registry_fields.IntraRegistryForeignKey(
         cgm_models.WifiRadioDeviceConfig, editable=False, null=False, related_name='antennas'
     )
-    antenna = registry_fields.ModelRegistryChoiceField(Antenna)
+    antenna = registry_fields.ModelRegistryChoiceField(Antenna, null=True, blank=True)
     azimuth = models.FloatField(null=True, blank=True)
     elevation_angle = models.FloatField(null=True, blank=True)
     rotation = models.FloatField(null=True, blank=True)
