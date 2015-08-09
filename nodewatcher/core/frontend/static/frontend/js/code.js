804137a9d6247b65abef5ed8a52bd3b1e86d9e5d
Luka Čehovin <luka.cehovin@gmail.com>
2015-08-10 00:29:44 +0200

    Adding theme JS icon generator.

--- nodewatcher/core/frontend/static/frontend/js/code.js
+++ nodewatcher/core/frontend/static/frontend/js/code.js
@@ -5,4 +5,18 @@
         // TODO: Handle erros better
         console.error(event, jqXHR, ajaxSettings, thrownError);
     });
+
+	if (!$.nodewatcher) { $.nodewatcher = {}; }
+	if (!$.nodewatcher.theme) { $.nodewatcher.theme = {}; }
+
+    $.extend($.nodewatcher.theme, {
+    	'iconElement': function(identifier, icons) {
+    		if (_.isUndefined(icons)) icons = 'nw';
+    		return $('div').addClass('icon ' + icons + ' ' + icons + '-' + identifier).clone().wrap('<i>').parent().html();
+    	},
+    	'iconHtml': function(identifier, icons) {
+    		return $.nodewatcher.theme.iconElement(identifier, icons).clone().wrap('<i>').parent().html();
+    	}
+    });
+
 })(jQuery);
