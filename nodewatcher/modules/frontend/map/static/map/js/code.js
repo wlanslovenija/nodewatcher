c1f111f2aae3087dadbbbdf4054e20568069ea73
Jernej Kos <jernej@kos.mx>
2015-09-20 18:42:55 +0200

    Optimize loading of topology stream.

--- nodewatcher/modules/frontend/map/static/map/js/code.js
+++ nodewatcher/modules/frontend/map/static/map/js/code.js
@@ -9,8 +9,9 @@
             'url': "/api/v1/stream/?format=json&tags__module=topology&limit=1",
         }).done(function(data) {
             var streamId = data.objects[0].id;
+            var latestTimestamp = moment(data.objects[0].latest_datapoint).unix();
             $.ajax({
-                'url': "/api/v1/stream/" + streamId + "/?format=json&reverse=true&limit=1",
+                'url': "/api/v1/stream/" + streamId + "/?format=json&reverse=true&limit=1&start_exclusive=" + latestTimestamp,
             }).done(function(data) {
                 var graph = data.datapoints[0].v;
                 var nodes = [];
