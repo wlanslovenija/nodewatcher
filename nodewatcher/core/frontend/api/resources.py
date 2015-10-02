6e434e325fce6cc9416709368bb82a8ae166a46b
Jernej Kos <jernej@kos.mx>
2015-10-02 10:25:49 +0200

    Don't include display_order in API.

--- nodewatcher/core/frontend/api/resources.py
+++ nodewatcher/core/frontend/api/resources.py
@@ -97,7 +97,7 @@ class BaseResource(six.with_metaclass(BaseMetaclass, resources.NamespacedModelRe
         if field.name.startswith('_'):
             return True
 
-        if field.name in ('registry_metadata', 'annotations'):
+        if field.name in ('registry_metadata', 'annotations', 'display_order'):
             return True
 
         if isinstance(field, (registry_fields.RegistryRelationField, registry_fields.RegistryMultipleRelationField)):
