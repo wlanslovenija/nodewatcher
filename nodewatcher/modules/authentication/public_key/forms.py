f619dd5ba4158c4d8427c8247b9414accbd964a7
Jernej Kos <jernej@kos.mx>
2015-10-12 22:21:34 +0200

    Always allow selection of already configured public keys.

--- nodewatcher/modules/authentication/public_key/forms.py
+++ nodewatcher/modules/authentication/public_key/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.db import models as django_models
 
 from nodewatcher.core.registry import registration
 
@@ -19,9 +20,17 @@ class PublicKeyAuthenticationConfigForm(forms.ModelForm):
         Dynamically modifies the form.
         """
 
-        # Only allow selection of user's public keys
+        # Only allow selection of user's public keys. Also, if there are any existing
+        # public keys set from other users, we should also allow those.
         qs = self.fields['public_key'].queryset
-        qs = qs.filter(user=request.user)
+        query = django_models.Q(user=request.user)
+        try:
+            if item.public_key.pk:
+                query |= django_models.Q(pk=item.public_key.pk)
+        except (models.UserAuthenticationKey.DoesNotExist, AttributeError):
+            pass
+
+        qs = qs.filter(query)
         self.fields['public_key'].queryset = qs
 
 registration.register_form_for_item(models.PublicKeyAuthenticationConfig, PublicKeyAuthenticationConfigForm)
