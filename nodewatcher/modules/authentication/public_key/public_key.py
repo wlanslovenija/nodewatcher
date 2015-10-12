5d890de036ebd31a9af0c1383f36b2363d307775
Jernej Kos <jernej@kos.mx>
2015-10-12 22:15:15 +0200

    Properly validate SSH public keys.

--- nodewatcher/modules/authentication/public_key/public_key.py
+++ nodewatcher/modules/authentication/public_key/public_key.py
@@ -1,6 +1,13 @@
 import hashlib
 
+from cryptography import exceptions as cryptography_exceptions
+from cryptography.hazmat import backends
+from cryptography.hazmat.primitives import serialization
+
 from . import exceptions
+
+# Ensure default cryptography backend is loaded.
+backends.default_backend()
 
 
 class PublicKey(object):
@@ -16,11 +23,18 @@ class PublicKey(object):
         """
 
         parts = key.split()
-        if len(parts) < 2 or len(parts) > 3:
+        if len(parts) != 2 and len(parts) != 3:
             raise exceptions.InvalidPublicKey
 
+        # Validate that the public key is actually a valid key.
         try:
-            self.key = parts[1].decode('base64')
+            self.key = serialization.load_ssh_public_key(key, backends.default_backend())
+        except (cryptography_exceptions.UnsupportedAlgorithm, ValueError):
+            raise exceptions.InvalidPublicKey
+
+        # Decode raw key in order to compute the fingerprint in a compatible way.
+        try:
+            self.raw_key = parts[1].decode('base64')
         except Exception:
             raise exceptions.InvalidPublicKey
 
@@ -29,5 +43,5 @@ class PublicKey(object):
         Returns a hex-encoded MD5 fingerprint of the public key.
         """
 
-        digest = hashlib.md5(self.key).hexdigest()
+        digest = hashlib.md5(self.raw_key).hexdigest()
         return ':'.join(a + b for a, b in zip(digest[::2], digest[1::2]))
