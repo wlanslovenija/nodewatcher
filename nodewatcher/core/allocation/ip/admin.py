9ff512b960c5b43dcf67b8bbf1d20f804286a011
Jernej Kos <jernej@kos.mx>
2015-10-17 20:06:05 +0200

    Order IP pools by description.

--- nodewatcher/core/allocation/ip/admin.py
+++ nodewatcher/core/allocation/ip/admin.py
@@ -4,6 +4,8 @@ from . import models
 
 
 class IpPoolAdmin(admin.ModelAdmin):
+    ordering = ('description',)
+
     def get_queryset(self, request):
         qs = super(IpPoolAdmin, self).get_queryset(request)
         # Hide non-top-level pools.
