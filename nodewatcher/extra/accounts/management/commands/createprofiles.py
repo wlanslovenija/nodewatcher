0f8f55f4170042387133f5ad0030772864399028
Mitar <mitar.git@tnode.com>
2015-11-21 14:20:25 -0800

    Ported createprofiles management command to newer Django style.

--- nodewatcher/extra/accounts/management/commands/createprofiles.py
+++ nodewatcher/extra/accounts/management/commands/createprofiles.py
@@ -1,8 +1,6 @@
-from django.core import management
 from django.core.management import base as management_base
 from django.contrib.auth import models as auth_models
 from django.db import transaction
-from django.db.models import signals as models_signals
 
 from ... import models
 
@@ -20,15 +18,8 @@ class Command(management_base.NoArgsCommand):
         """
 
         verbosity = int(options.get('verbosity', 1))
-        with transaction.atomic():
+        with transaction.atomic(using=options.get('using', None)):
             for user in auth_models.User.objects.all():
-                profile, created = models.UserProfileAndSettings.objects.get_or_create(user=user)
+                profile, created = models.UserProfileAndSettings.objects.get_or_create(user=user, using=options.get('using', None))
                 if verbosity == 2 and created:
                     self.stdout.write('Created %s.\n' % profile)
-
-
-def command_signal(sender, app, created_models, **kwargs):
-    if models.UserProfileAndSettings in created_models:
-        management.call_command('createprofiles', **kwargs)
-
-models_signals.post_syncdb.connect(command_signal, sender=models)
