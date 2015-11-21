4ce69c8b842ac9eb4444c9ee32ed152bb0138673
Mitar <mitar.git@tnode.com>
2015-11-21 14:13:22 -0800

    Move accounts signal connection to the app.

---
+++ nodewatcher/extra/accounts/apps.py
@@ -0,0 +1,17 @@
+from django import apps
+from django.core import management
+from django.db.models import signals as models_signals
+
+
+def command_signal(sender, **kwargs):
+    management.call_command('createprofiles', **kwargs)
+
+
+class AccountsConfig(apps.AppConfig):
+    name = 'nodewatcher.extra.accounts'
+    label = 'accounts'
+
+    def ready(self):
+        super(AccountsConfig, self).ready()
+
+        models_signals.post_migrate.connect(command_signal, sender=self)
