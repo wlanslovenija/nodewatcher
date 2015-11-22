a144eb95dc527c83354e742ccf7b6ba54b717548
Mitar <mitar.git@tnode.com>
2015-11-21 23:07:30 -0800

    Style.

--- nodewatcher/extra/accounts/validators.py
+++ nodewatcher/extra/accounts/validators.py
@@ -1,7 +1,7 @@
 from django.core import exceptions, validators
 from django.utils.translation import ugettext_lazy as _
 
-import dns.resolver
+from dns import resolver
 
 
 def validate_email_with_hostname(value):
@@ -9,23 +9,23 @@ def validate_email_with_hostname(value):
     Validator for e-mail addresses which does not only check for valid format but also checks for existence of e-mail address' hostname.
     """
 
-    # First we run core e-mail validator
+    # First we run core e-mail validator.
     validators.validate_email(value)
-    # It seems it looks like e-mail address, let us check hostname
+    # It seems it looks like e-mail address, let us check hostname.
     if value and u'@' in value:
         parts = value.split(u'@')
         hostname_part = parts[-1]
 
         try:
             # Try to resolve it
-            dns.resolver.query(hostname_part, 'MX')
+            resolver.query(hostname_part, 'MX')
             return
         except:
             pass
 
         try:
-            # If MX record does not exist, SMTP fails back to A (or AAAA) records
-            dns.resolver.query(hostname_part)
+            # If MX record does not exist, SMTP fails back to A (or AAAA) records.
+            resolver.query(hostname_part)
             return
         except:
             pass
