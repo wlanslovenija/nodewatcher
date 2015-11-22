46e509588c86c9128b9668aae812fb89d2e66eee
Philip Berndroth <p.berndroth@philuweb.de>
2015-11-22 17:50:05 +0100

    Added TP-Link WDR3600 v1 device descriptor.

--- nodewatcher/modules/devices/tplink/__init__.py
+++ nodewatcher/modules/devices/tplink/__init__.py
@@ -1,2 +1,2 @@
 # TP-Link device modules
-from . import wr703n, wr740nd, wr741nd, wr743nd, wr841nd, wr842nd, wr941nd, wr1041nd, wr1043nd, mr3020, mr3040, wdr4300, wr843nd
+from . import wr703n, wr740nd, wr741nd, wr743nd, wr841nd, wr842nd, wr941nd, wr1041nd, wr1043nd, mr3020, mr3040, wdr4300, wr843nd, wdr3600
