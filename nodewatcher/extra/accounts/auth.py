400eeafea9699ce8598cc807d86ae9c31268bf51
Mitar <mitar.git@tnode.com>
2015-11-21 21:57:14 -0800

    Updated authentication backends to newer Django.

--- nodewatcher/extra/accounts/auth.py
+++ nodewatcher/extra/accounts/auth.py
@@ -1,6 +1,7 @@
 import crypt
 import md5crypt
 
+from django.contrib import auth
 from django.contrib.auth import backends as auth_backends, models as auth_models
 
 try:
@@ -16,7 +17,7 @@ class AprBackend(object):
     supports_anonymous_user = False
     supports_inactive_user = False
 
-    def authenticate(self, username=None, password=None):
+    def authenticate(self, username=None, password=None, **kwargs):
         """
         Authenticates against the database using Apache Portable Runtime MD5 hash function.
         """
@@ -24,26 +25,35 @@ class AprBackend(object):
         if not APR_ENABLED:
             return None
 
+        UserModel = auth.get_user_model()
+        if username is None:
+            username = kwargs.get(UserModel.USERNAME_FIELD)
         try:
-            user = auth_models.User.objects.get(username__iexact=username)
+            user = UserModel._default_manager.get(**{('%s__iexact' % UserModel.USERNAME_FIELD): username})
             if aprmd5.password_validate(password, user.password):
-                # Successfully checked password, so we change it to the Django password format
+                # Successfully checked password, so we change it to the Django password format.
                 user.set_password(password)
                 user.save()
                 return user
         except ValueError:
-            return None
-        except auth_models.User.DoesNotExist:
-            return None
+            pass
+        except UserModel.DoesNotExist:
+            pass
+
+        # Run the default password hasher once to reduce the timing
+        # difference between an existing and a non-existing user (#20760).
+        UserModel().set_password(password)
+        return None
 
     def get_user(self, user_id):
         """
         Translates the user ID into the User object.
         """
 
+        UserModel = auth.get_user_model()
         try:
-            return auth_models.User.objects.get(pk=user_id)
-        except auth_models.User.DoesNotExist:
+            return UserModel._default_manager.get(pk=user_id)
+        except UserModel.DoesNotExist:
             return None
 
 
@@ -52,45 +62,63 @@ class CryptBackend(object):
     supports_anonymous_user = False
     supports_inactive_user = False
 
-    def authenticate(self, username=None, password=None):
+    def authenticate(self, username=None, password=None, **kwargs):
         """
         Authenticates against the database using crypt hash function.
         """
 
+        UserModel = auth.get_user_model()
+        if username is None:
+            username = kwargs.get(UserModel.USERNAME_FIELD)
         try:
-            user = auth_models.User.objects.get(username__iexact=username)
+            user = UserModel._default_manager.get(**{('%s__iexact' % UserModel.USERNAME_FIELD): username})
             if crypt.crypt(password, user.password) == user.password or md5crypt.md5crypt(password, user.password) == user.password:
-                # Successfully checked password, so we change it to the Django password format
+                # Successfully checked password, so we change it to the Django password format.
                 user.set_password(password)
                 user.save()
                 return user
         except ValueError:
-            return None
-        except auth_models.User.DoesNotExist:
-            return None
+            pass
+        except UserModel.DoesNotExist:
+            pass
+
+        # Run the default password hasher once to reduce the timing
+        # difference between an existing and a non-existing user (#20760).
+        UserModel().set_password(password)
+        return None
 
     def get_user(self, user_id):
         """
         Translates the user ID into the User object.
         """
 
+        UserModel = auth.get_user_model()
         try:
-            return auth_models.User.objects.get(pk=user_id)
-        except auth_models.User.DoesNotExist:
+            return UserModel._default_manager.get(pk=user_id)
+        except UserModel.DoesNotExist:
             return None
 
 
 class ModelBackend(auth_backends.ModelBackend):
-    def authenticate(self, username=None, password=None):
+    def authenticate(self, username=None, password=None, **kwargs):
         """
-        Authenticates against the database using official implementation but catches exceptions and does it in case-insensitive manner.
+        Authenticates against the database using official implementation but
+        catches exceptions and does it in case-insensitive manner.
         """
 
+        UserModel = auth.get_user_model()
+        if username is None:
+            username = kwargs.get(UserModel.USERNAME_FIELD)
         try:
-            user = auth_models.User.objects.get(username__iexact=username)
+            user = UserModel._default_manager.get(**{('%s__iexact' % UserModel.USERNAME_FIELD): username})
             if user.check_password(password):
                 return user
         except ValueError:
-            return None
-        except auth_models.User.DoesNotExist:
-            return None
+            pass
+        except UserModel.DoesNotExist:
+            pass
+
+        # Run the default password hasher once to reduce the timing
+        # difference between an existing and a non-existing user (#20760).
+        UserModel().set_password(password)
+        return None
