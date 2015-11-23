d5dfc8fe232bb674069f281cdbfb399704366b09
Mitar <mitar.git@tnode.com>
2015-11-22 22:30:10 -0800

    Create node maintainers group automatically.

--- nodewatcher/extra/accounts/apps.py
+++ nodewatcher/extra/accounts/apps.py
@@ -1,5 +1,6 @@
-from django import apps
+from django import apps, db
 from django.core import management
+from django.contrib.auth import models as auth_models
 from django.db.models import signals as models_signals
 
 
@@ -7,13 +8,28 @@ class AccountsConfig(apps.AppConfig):
     name = 'nodewatcher.extra.accounts'
     label = 'accounts'
 
-    def createprofiles(self, **kwargs):
+    def create_profiles(self, **kwargs):
         management.call_command('createprofiles', **kwargs)
+
+    def create_node_maintainers_group(self, **kwargs):
+        try:
+            # We try to create group object so that it always exist.
+            group = auth_models.Group.objects.create(name="Node maintainers")
+
+            # And assign the group "add_node" permission.
+            permission = auth_models.Permission.objects.get_by_natural_key(codename='add_node', app_label='core', model='node')
+            group.permissions.add(permission)
+        except db.IntegrityError:
+            pass
+        except auth_models.Permission.DoesNotExist:
+            pass
 
     def ready(self):
         super(AccountsConfig, self).ready()
 
-        models_signals.post_migrate.connect(self.createprofiles, sender=self)
+        models_signals.post_migrate.connect(self.create_profiles, sender=self)
+
+        models_signals.post_migrate.connect(self.create_node_maintainers_group, sender=self)
 
         # Register module signals.
         from . import signals
