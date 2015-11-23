9d5604ca77fc5b023d6851a7fe3bf5eb6be75823
Mitar <mitar.git@tnode.com>
2015-11-22 21:32:28 -0800

    Refactoring.

--- nodewatcher/extra/accounts/apps.py
+++ nodewatcher/extra/accounts/apps.py
@@ -3,18 +3,17 @@ from django.core import management
 from django.db.models import signals as models_signals
 
 
-def command_signal(sender, **kwargs):
-    management.call_command('createprofiles', **kwargs)
-
-
 class AccountsConfig(apps.AppConfig):
     name = 'nodewatcher.extra.accounts'
     label = 'accounts'
 
+    def createprofiles(self, **kwargs):
+        management.call_command('createprofiles', **kwargs)
+
     def ready(self):
         super(AccountsConfig, self).ready()
 
-        models_signals.post_migrate.connect(command_signal, sender=self)
+        models_signals.post_migrate.connect(self.createprofiles, sender=self)
 
         # Register module signals.
         from . import signals
