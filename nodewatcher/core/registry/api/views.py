7cd334de38001e1f282fe6628c71d2eec5b31817
Jernej Kos <jernej@kos.mx>
2016-03-12 20:29:44 +0100

    Implemented filters.

--- nodewatcher/core/registry/api/views.py
+++ nodewatcher/core/registry/api/views.py
@@ -1,3 +1,5 @@
+from django.db.models import constants
+
 from rest_framework import viewsets
 
 FIELD_SEPARATOR = '|'
@@ -9,9 +11,45 @@ class RegistryRootViewSetMixin(object):
         return self.get_registry_queryset(queryset)
 
     def get_registry_queryset(self, queryset):
+        # Apply projection.
         for field in self.request.query_params.getlist('fields'):
             field_name, queryset = apply_registry_field(field, queryset)
+
+        # Apply filters.
+        for argument, value in self.request.query_params.items():
+            if argument in ('fields', 'limit', 'offset'):
+                continue
+
+            queryset = apply_registry_filter(argument, value, queryset)
+
         return queryset
+
+
+def apply_registry_filter(path, value, queryset):
+    """
+    Applies a given registry filter to the given queryset.
+
+    :param path: Field path
+    :param value: Filtered value
+    :param queryset: Queryset to apply the filter to
+    :return: An updated queryset
+    """
+
+    atoms = path.split(FIELD_SEPARATOR)
+
+    # Parse registry point.
+    if atoms:
+        registry_point = atoms.pop(0)
+        queryset = queryset.regpoint(registry_point)
+
+        # Parse registry item identifier.
+        if atoms:
+            registry_id = atoms.pop(0)
+            registry_id = registry_id.replace('.', '_')
+            condition = '%s__%s' % (registry_id, constants.LOOKUP_SEP.join(atoms))
+            queryset = queryset.registry_filter(**{condition: value})
+
+    return queryset
 
 
 def apply_registry_field(field_specifier, queryset):
@@ -31,16 +69,16 @@ def apply_registry_field(field_specifier, queryset):
         registry_point = atoms.pop(0)
         queryset = queryset.regpoint(registry_point)
 
-    # Parse registry item identifier.
-    if atoms:
-        registry_id = atoms.pop(0)
-
-        # Parse optional field path specifier.
-        field_path = ''
+        # Parse registry item identifier.
         if atoms:
-            field_path = atoms.pop(0)
+            registry_id = atoms.pop(0)
 
-        field_name = '%s%s%s' % (registry_point, registry_id.replace('.', '_'), field_path.replace('.', '_'))
-        queryset = queryset.registry_fields(**{field_name: '%s%s' % (registry_id, ('#%s' % field_path) if field_path else '')})
+            # Parse optional field path specifier.
+            field_path = ''
+            if atoms:
+                field_path = atoms.pop(0)
+
+            field_name = '%s%s%s' % (registry_point, registry_id.replace('.', '_'), field_path.replace('.', '_'))
+            queryset = queryset.registry_fields(**{field_name: '%s%s' % (registry_id, ('#%s' % field_path) if field_path else '')})
 
     return field_name, queryset
