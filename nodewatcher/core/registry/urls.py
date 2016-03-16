3e0edb01db114a6989655324e3b9bc1c9d299009
Jernej Kos <jernej@kos.mx>
2016-03-16 09:46:12 +0100

    registry: Fixed URLs.
    Broken in commit abcf1c601ba9e076d9734e738a991e1576bd2722.

--- nodewatcher/core/registry/urls.py
+++ nodewatcher/core/registry/urls.py
@@ -3,5 +3,5 @@ from django.conf import urls
 from . import views
 
 urlpatterns = [
-    urls.url(r'evaluate_forms/(?P<regpoint_id>.*)/(?P<root_id>.*)/$', views.evaluate_forms, name='evaluate_forms'),
+    urls.url(r'evaluate_forms/(?P<regpoint_id>.+?)/(?:(?P<root_id>.+?)/)?$', views.evaluate_forms, name='evaluate_forms'),
 ]

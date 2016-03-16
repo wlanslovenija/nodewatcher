wlanslovenija/nodewatcher

commit abcf1c601ba9e076d9734e738a991e1576bd2722

Use trailing `/` in URLs.

nodewatcher/core/frontend/static/frontend/js/registry.js
@@ -168,7 +168,7 @@
         forms += '&ACTIONS=' + encodeURI(JSON.stringify(actions));
 
         $.postCsrf(
-            "/registry/evaluate_forms/" + regpoint_id + "/" + root_id,
+            "/registry/evaluate_forms/" + regpoint_id + "/" + root_id + "/",
             forms
         ).done(function(data) {
             $('#registry_forms').html(data);

nodewatcher/core/registry/urls.py
@@ -3,5 +3,5 @@ from django.conf import urls
 from . import views
 
 urlpatterns = [
-    urls.url(r'evaluate_forms/(?P<regpoint_id>.*)/(?P<root_id>.*)$', views.evaluate_forms, name='evaluate_forms'),
+    urls.url(r'evaluate_forms/(?P<regpoint_id>.*)/(?P<root_id>.*)/$', views.evaluate_forms, name='evaluate_forms'),
 ]

nodewatcher/modules/authentication/public_key/frontend.py
@@ -10,9 +10,9 @@ class PublicKeyComponent(components.FrontendComponent):
     @classmethod
     def get_urls(cls):
         return super(PublicKeyComponent, cls).get_urls() + [
-            urls.url(r'^my/public_keys$', views.ListPublicKeys.as_view(), name='list'),
-            urls.url(r'^my/public_keys/add$', views.AddPublicKey.as_view(), name='add'),
-            urls.url(r'^my/public_keys/(?P<pk>[^/]+)/remove$', views.RemovePublicKey.as_view(), name='remove'),
+            urls.url(r'^my/public_keys/$', views.ListPublicKeys.as_view(), name='list'),
+            urls.url(r'^my/public_keys/add/$', views.AddPublicKey.as_view(), name='add'),
+            urls.url(r'^my/public_keys/(?P<pk>[^/]+)/remove/$', views.RemovePublicKey.as_view(), name='remove'),
         ]
 
 components.pool.register(PublicKeyComponent)

nodewatcher/modules/frontend/generator/frontend.py
@@ -12,8 +12,8 @@ class GeneratorComponent(components.FrontendComponent):
         return super(GeneratorComponent, cls).get_urls() + [
             urls.url(r'^node/(?P<pk>[^/]+)/generate_firmware/$', views.GenerateFirmware.as_view(), name='generate_firmware'),
 
-            urls.url(r'^my/builds$', views.ListBuilds.as_view(), name='list_builds'),
-            urls.url(r'^my/builds/(?P<pk>[^/]+)$', views.ViewBuild.as_view(), name='view_build'),
+            urls.url(r'^my/builds/$', views.ListBuilds.as_view(), name='list_builds'),
+            urls.url(r'^my/builds/(?P<pk>[^/]+)/$', views.ViewBuild.as_view(), name='view_build'),
         ]
 
 components.pool.register(GeneratorComponent)

nodewatcher/modules/monitor/sources/http/frontend.py
@@ -10,7 +10,7 @@ class HttpPushComponent(components.FrontendComponent):
     def get_urls(cls):
         return super(HttpPushComponent, cls).get_urls() + [
             # Push endpoint.
-            urls.url(r'^push/http/(?P<uuid>.+)$', views.HttpPushEndpoint.as_view(), name='endpoint'),
+            urls.url(r'^push/http/(?P<uuid>.+)/$', views.HttpPushEndpoint.as_view(), name='endpoint'),
         ]
 
 components.pool.register(HttpPushComponent)

nodewatcher/modules/monitor/unknown_nodes/frontend.py
@@ -10,8 +10,8 @@ class UnknownNodesComponent(components.FrontendComponent):
     @classmethod
     def get_urls(cls):
         return super(UnknownNodesComponent, cls).get_urls() + [
-            urls.url(r'^my/unknown_nodes$', views.ListUnknownNodes.as_view(), name='list'),
-            urls.url(r'^my/unknown_nodes/register/(?P<uuid>[^/]+)$', views.RegisterUnknownNode.as_view(), name='register'),
+            urls.url(r'^my/unknown_nodes/$', views.ListUnknownNodes.as_view(), name='list'),
+            urls.url(r'^my/unknown_nodes/register/(?P<uuid>[^/]+)/$', views.RegisterUnknownNode.as_view(), name='register'),
         ]
 
 components.pool.register(UnknownNodesComponent)

commit 3e0edb01db114a6989655324e3b9bc1c9d299009

registry: Fixed URLs.
Broken in commit abcf1c601ba9e076d9734e738a991e1576bd2722.

nodewatcher/core/frontend/static/frontend/js/registry.js
@@ -167,9 +167,14 @@
         var forms = $('#registry_forms *').serialize();
         forms += '&ACTIONS=' + encodeURI(JSON.stringify(actions));
 
+        // TODO: Dynamically resolve the URL and do not have it hard-coded.
+        var registryEndpoint = '/registry/evaluate_forms/' + regpoint_id + '/';
+        if (root_id) {
+            registryEndpoint += root_id + '/';
+        }
+
         $.postCsrf(
-            // TODO: Dynamically resolve the URL and do not have it hard-coded.
-            "/registry/evaluate_forms/" + regpoint_id + "/" + root_id + "/",
+            registryEndpoint,
             forms
         ).done(function(data) {
             $('#registry_forms').html(data);

nodewatcher/core/registry/urls.py
@@ -3,5 +3,5 @@ from django.conf import urls
 from . import views
 
 urlpatterns = [
-    urls.url(r'evaluate_forms/(?P<regpoint_id>.*)/(?P<root_id>.*)/$', views.evaluate_forms, name='evaluate_forms'),
+    urls.url(r'evaluate_forms/(?P<regpoint_id>.+?)/(?:(?P<root_id>.+?)/)?$', views.evaluate_forms, name='evaluate_forms'),
 ]