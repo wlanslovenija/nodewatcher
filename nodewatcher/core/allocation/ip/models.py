adbac90a833594b9ff96af2cd94f65ea13c33c4f
Mitar <mitar.git@tnode.com>
2016-04-24 04:23:51 -0700

    Added TODOs for /31 subnets.

--- nodewatcher/core/allocation/ip/models.py
+++ nodewatcher/core/allocation/ip/models.py
@@ -97,6 +97,9 @@ class IpPool(allocation_models.PoolBase):
         if not self.parent:
             self.reclaim_held_down()
 
+        # TODO: Consider relaxing this requirement to allow point-to-point links as specified in RFC 3021.
+        #       /31 subnets are not really reasonable (because you need network and broadcast address, and then
+        #       non are left), but we might allow this in the future fore point-to-point links.
         if prefix_len == 31:
             return None
 
@@ -315,6 +318,9 @@ class IpPool(allocation_models.PoolBase):
         if prefix_len < self.prefix_length_minimum or prefix_len > self.prefix_length_maximum:
             return None
 
+        # TODO: Consider relaxing this requirement to allow point-to-point links as specified in RFC 3021.
+        #       /31 subnets are not really reasonable (because you need network and broadcast address, and then
+        #       non are left), but we might allow this in the future fore point-to-point links.
         if prefix_len == 31:
             return None
 
