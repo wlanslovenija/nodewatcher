b80cdc1128199c8532dde1b5ff40dc748a10e32a
masha-len-ub-g-i3 <david.marn@gmail.com>
2016-06-07 12:54:50 -0700

    Changed the name of the function to register packages for wireless metadata.

--- nodewatcher/modules/monitor/http/survey/cgm.py
+++ nodewatcher/modules/monitor/http/survey/cgm.py
@@ -2,7 +2,7 @@ from nodewatcher.core.generator.cgm import base as cgm_base
 
 
 @cgm_base.register_platform_module('openwrt', 50)
-def clients(node, cfg):
+def wireless(node, cfg):
     """
     Register packages needed for reporting wireless metadata.
     """
