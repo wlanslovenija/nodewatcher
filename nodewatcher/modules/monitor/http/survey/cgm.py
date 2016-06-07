3d69be5e4f3b619b650e09630bc1e33b1b199102
masha-len-ub-g-i3 <david.marn@gmail.com>
2016-06-07 12:52:35 -0700

    Improved clarity of documentation for the required packages.

--- nodewatcher/modules/monitor/http/survey/cgm.py
+++ nodewatcher/modules/monitor/http/survey/cgm.py
@@ -4,7 +4,7 @@ from nodewatcher.core.generator.cgm import base as cgm_base
 @cgm_base.register_platform_module('openwrt', 50)
 def clients(node, cfg):
     """
-    Register packages needed for reporting the clients.
+    Register packages needed for reporting wireless metadata.
     """
 
     cfg.packages.update(['nodewatcher-agent-mod-wireless'])
