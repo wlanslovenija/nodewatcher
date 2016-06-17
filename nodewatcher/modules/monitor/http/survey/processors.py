a2f518f050f628f9f3c246ae6a06c2dd1b8ffe13
masha-len-ub-g-i3 <david.marn@gmail.com>
2016-06-17 12:01:42 -0700

    Made style changed and improved the clarity of the comments.

--- nodewatcher/modules/monitor/http/survey/processors.py
+++ nodewatcher/modules/monitor/http/survey/processors.py
@@ -10,14 +10,19 @@ from nodewatcher.modules.monitor.datastream.pool import pool as ds_pool
 
 class SurveyInfoStreams(ds_models.RegistryRootStreams):
     neighbor_graph = ds_fields.GraphField(tags={
-        'title': _("Neighbor topology"),
-        'description': _("Neighbor topology."),
+        'title': _("Neighborhood topology"),
+        'description': _("Neighborhood topology."),
     })
 
     def get_module_name(self):
         return 'monitor.http.survey'
 
     def get_stream_highest_granularity(self):
+        # The nodewatcher-agent performs a survey once every ~240 monitoring intervals according to
+        # https://github.com/wlanslovenija/nodewatcher-agent/blob/master/modules/wireless.c#L362
+        # and a monitoring run is performed every 30 seconds according to
+        # https://github.com/wlanslovenija/nodewatcher-agent/blob/73f2b25db2c34ae7d70904bac31379a2243bade0/modules/wireless.c#L437.
+        # So a survey is performed once every two hours, meaning that we use hourly granularity.
         return datastream.Granularity.Hours
 
 
@@ -35,10 +40,11 @@ ds_pool.register(SurveyInfoStreamsData, SurveyInfoStreams)
 
 class SurveyInfo(monitor_processors.NodeProcessor):
     """
-    Stores neighbor's reported channel, BSSID, SSID and signal strength into the datastream. A star shaped graph
-    is constructed. The source vertex contains an array of all BSSIDs of that node. Source vertex is stored according
-    to its UUID, wherease all neighbors are stored according to their BSSIDs. Will only run if HTTP monitor module
-    has previously fetched data.
+    Accesses the data collected during a WiFi survey of neighboring wireless access points / routers. Stores channel,
+    BSSID, SSID and signal strength for each neighbor into the datastream. A star shaped graph is constructed. The
+    source vertex contains an array of all BSSIDs of that node. Source vertex is stored according to its UUID,
+    wherease all neighbors are stored according to their BSSIDs. Will only run if HTTP monitor module has previously
+    fetched data.
     """
 
     @monitor_processors.depends_on_context("http", http_processors.HTTPTelemetryContext)
@@ -55,17 +61,19 @@ class SurveyInfo(monitor_processors.NodeProcessor):
         if version == 0:
             # Unsupported version or data fetch failed
             return context
+
         edges, vertices = [], []
         try:
-            vertex_bssids = []
-            for interface in context.http.core.wireless.interfaces.values():
-                vertex_bssids.append(interface['bssid'])
-            vertex = dict(i=str(node.uuid), bssids=vertex_bssids)
-            vertices.append(vertex)
+            source_vertex_bssids = [interface.bssid for interface in context.http.core.wireless.interfaces.values()]
+            source_vertex = {
+                'i': str(node.uuid),
+                'bssids': source_vertex_bssids,
+            }
+            vertices.append(source_vertex)
             for radio in context.http.core.wireless.radios.values():
                 for neighbor in radio.survey:
-                    vertices.append(dict(i=neighbor['bssid']))
-                    # Add an edge for that neighbor
+                    vertices.append({'i': neighbor['bssid']})
+                    # Add an edge for that neighbor.
                     edge = {'f': str(node.uuid), 't': neighbor['bssid']}
                     edge['channel'] = neighbor['channel']
                     edge['signal'] = neighbor['signal']
@@ -74,7 +82,8 @@ class SurveyInfo(monitor_processors.NodeProcessor):
         except KeyError:
             pass
         except NameError:
-            self.logger.warning("Error parsing JSON file for " + str(node))
+            self.logger.warning("Could not parse survey data for node: " + str(node))
+
         # Store survey graph into datastream.
         context.datastream.survey_topology = SurveyInfoStreamsData(node, vertices, edges)
         return context
