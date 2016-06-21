1cfb818b135e82a972b5dccb263b462b9b1267e5
Jernej Kos <jernej@kos.mx>
2016-06-21 15:23:00 +0200

    datastream: Add stream copy utility function.

---
+++ nodewatcher/modules/monitor/datastream/utils.py
@@ -0,0 +1,106 @@
+import datetime
+
+import datastream
+
+from nodewatcher.utils import toposort
+
+# Number of datapoints to commit in one batch.
+COMMIT_BATCH_SIZE = 1000
+
+
+def datastream_copy(source, destination):
+    """
+    Copies all streams from one datastream backend to another. Source and destination
+    MUST differ. All data in destination datastream backend WILL BE LOST.
+
+    :param source: Source datastream backend instance
+    :param destination: Destination datastream backend instance
+    """
+
+    # Do a basic check if source and destination are the same. This cannot determine
+    # if they are actually different in which case the copy will fail and all source
+    # data will be lost.
+    if source == destination:
+        raise ValueError('Source and destination must differ!')
+
+    ds_source = datastream.Datastream(source)
+    ds_destination = datastream.Datastream(destination)
+
+    # Load all stream metadata into memory.
+    print "Loading streams from source."
+    streams = {}
+    for stream in ds_source.find_streams():
+        streams[stream['stream_id']] = {
+            'stream': stream,
+            'dependencies': stream.get('derived_from', {}).get('stream_ids', []),
+        }
+
+    # Topologically sort the streams based on dependencies.
+    print "Resolving dependencies with %d streams." % len(streams)
+    sorted_streams = toposort.topological_sort(streams)
+
+    print "Dropping destination streams."
+    ds_destination.delete_streams()
+
+    print "Importing streams."
+    stream_no = 1
+    stream_map = {}
+    for stream_batch in sorted_streams:
+        for stream in stream_batch:
+            print "[%d/%d] Importing stream." % (stream_no, len(streams))
+
+            stream = datastream.Stream(stream['stream'])
+            value_downsamplers = list(set(stream.value_downsamplers).intersection(ds_destination.backend.value_downsamplers))
+
+            # Create the same stream on the destination.
+            kwargs = {}
+            if hasattr(stream, 'derived_from'):
+                # Resolve source streams.
+                kwargs['derive_from'] = [{'stream': stream_map[derived_id]} for derived_id in stream.derived_from['stream_ids']]
+                kwargs['derive_op'] = stream.derived_from['op']
+                kwargs['derive_args'] = stream.derived_from['args']
+
+                if stream.derived_from['op'] == 'counter_derivative':
+                    kwargs['derive_from'][0]['name'] = 'reset'
+
+            stream_id = ds_destination.ensure_stream(
+                query_tags={'import_id': stream.id},
+                tags=stream.tags,
+                value_downsamplers=value_downsamplers,
+                highest_granularity=stream.highest_granularity,
+                value_type=stream.value_type,
+                value_type_options=stream.value_type_options,
+                **kwargs
+            )
+
+            stream_map[stream.id] = stream_id
+
+            if not hasattr(stream, 'derived_from'):
+                # Copy data.
+                print "Copying data."
+                batch = []
+
+                try:
+                    for datapoint in ds_source.get_data(stream.id, stream.highest_granularity, start=datetime.datetime.min):
+                        batch.append({'stream_id': stream_id, 'value': datapoint['v'], 'timestamp': datapoint['t']})
+                        if len(batch) >= COMMIT_BATCH_SIZE:
+                            ds_destination.append_multiple(batch)
+                            batch = []
+
+                    if batch:
+                        ds_destination.append_multiple(batch)
+                        batch = []
+                except:
+                    print "ERROR: Failed to copy data."
+                    print "ERROR: Last batch was:"
+                    for value in batch:
+                        print "  %s" % value['value']
+                    print "ERROR: Aborting due to exception."
+                    raise
+
+            stream_no += 1
+
+    print "Backprocessing streams."
+    ds_destination.backprocess_streams()
+
+    print "Imported %d/%d streams." % (len(ds_destination.find_streams()), stream_no - 1)
