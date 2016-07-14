3769f91ac2addb670f52320a54b237f1eb573787
masha-len-ub-g-i3 <david.marn@gmail.com>
2016-07-13 19:55:51 -0700

    Created a function that can be called outside of the command.

--- nodewatcher/modules/monitor/http/survey/management/commands/export_survey_data.py
+++ nodewatcher/modules/monitor/http/survey/management/commands/export_survey_data.py
@@ -46,54 +46,13 @@ class Command(base.BaseCommand):
         """
 
         timestamp = options['timestamp']
-        if timestamp:
-            latest_survey_datetime = datetime.datetime.fromtimestamp(timestamp)
-        else:
-            latest_survey_datetime = datetime.datetime.utcnow()
-
         filename = options['filename']
 
-        streams = datastream.find_streams({'module': 'monitor.http.survey'})
+        exported_graph = extract_survey_graph(timestamp)
 
-        # Create a meta graph
-        meta_vertices, meta_edges = [], []
-        friendly_nodes = []
-        latest_data_timepoint = 0
-
-        for stream in streams:
-            datapoint_iterator = datastream.get_data(
-                stream_id=stream['stream_id'],
-                granularity=stream['highest_granularity'],
-                start=(latest_survey_datetime - datetime.timedelta(hours=2)),
-                reverse=True,
-            )
-            try:
-                stream_graph = datapoint_iterator[0]['v']
-                for vertex in stream_graph['v']:
-                    meta_vertices.append(vertex)
-                    if 'b' in vertex:
-                        friendly_nodes.append(vertex)
-                for edge in stream_graph['e']:
-                    meta_edges.append(edge)
-                if not latest_data_timepoint or datapoint_iterator[0]['t'] > latest_data_timepoint:
-                    latest_data_timepoint = datapoint_iterator[0]['t']
-            except IndexError:
-                pass
-
-        if not meta_vertices or not meta_edges:
+        if not exported_graph:
             self.stdout.write(self.style.ERROR("Insufficient survey data in the datastream during this time period."))
             return
-
-        meta_graph = {
-            'v': meta_vertices,
-            'e': meta_edges,
-        }
-
-        exported_graph = {
-            'graph': meta_graph,
-            'friendly_nodes': friendly_nodes,
-            'timepoint': str(latest_data_timepoint)
-        }
 
         if options['store_timestamp']:
             filename = '{0}{1}.json'.format(filename, timestamp)
@@ -104,3 +63,63 @@ class Command(base.BaseCommand):
             f.write(json.dumps(exported_graph, ensure_ascii=False))
 
         self.stdout.write(self.style.SUCCESS("Successfully exported survey data."))
+
+
+def extract_survey_graph(timestamp=None):
+    """
+    Extracts a graph of the survey during a specified time period. Only the latest data
+    from specified datetime and two hours preceding the datetime will be used.
+
+    :param timestamp: UNIX timestamp according to the UTC timezone.
+    :return: A dictionary that contains the graph under the 'graph' key.
+    """
+
+    if timestamp:
+        latest_survey_datetime = datetime.datetime.fromtimestamp(timestamp)
+    else:
+        latest_survey_datetime = datetime.datetime.utcnow()
+
+    streams = datastream.find_streams({'module': 'monitor.http.survey'})
+
+    # Create a meta graph
+    meta_vertices, meta_edges = [], []
+    friendly_nodes = []
+    latest_data_timepoint = 0
+
+    for stream in streams:
+        datapoint_iterator = datastream.get_data(
+            stream_id=stream['stream_id'],
+            granularity=stream['highest_granularity'],
+            start=(latest_survey_datetime - datetime.timedelta(hours=2)),
+            reverse=True,
+        )
+        try:
+            stream_graph = datapoint_iterator[0]['v']
+            for vertex in stream_graph['v']:
+                meta_vertices.append(vertex)
+                if 'b' in vertex:
+                    friendly_nodes.append(vertex['i'])
+                    for bssid in vertex['b']:
+                        friendly_nodes.append(bssid)
+            for edge in stream_graph['e']:
+                meta_edges.append(edge)
+            if not latest_data_timepoint or datapoint_iterator[0]['t'] > latest_data_timepoint:
+                latest_data_timepoint = datapoint_iterator[0]['t']
+        except IndexError:
+            pass
+
+    if not meta_vertices or not meta_edges:
+        return
+
+    meta_graph = {
+        'v': meta_vertices,
+        'e': meta_edges,
+    }
+
+    exported_graph = {
+        'graph': meta_graph,
+        'friendly_nodes': friendly_nodes,
+        'timepoint': str(latest_data_timepoint)
+    }
+
+    return exported_graph
