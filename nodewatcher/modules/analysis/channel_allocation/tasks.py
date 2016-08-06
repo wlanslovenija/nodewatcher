c6a08c3c9bf09aa0f79387411d55fa56b6e5c6c6
masha-len-ub-g-i3 <david.marn@gmail.com>
2016-08-05 17:49:15 -0700

    Implemented the allocation task skeleton.

---
+++ nodewatcher/modules/analysis/channel_allocation/tasks.py
@@ -0,0 +1,34 @@
+import datetime
+
+from nodewatcher import celery
+
+from nodewatcher.modules.monitor.http.survey import extract_nodes
+from nodewatcher.modules.analysis.channel_allocation import allocation_algorithms
+
+
+# Register the periodic schedule.
+celery.app.conf.CELERYBEAT_SCHEDULE['nodewatcher.modules.analysis.channel_allocation.tasks.allocation'] = {
+    'task': 'nodewatcher.modules.analysis.channel_allocation.tasks.allocation',
+    'schedule': datetime.timedelta(days=1),
+}
+
+
+@celery.app.task(queue='monitor', bind=True)
+def allocation(self):
+    """
+    Assigns an optiomal channel to each known node in the graph to maximize spectral efficiency.
+    """
+
+    extracted_graph = extract_nodes.all_nodes_survey_graph(datetime.datetime.utcnow())
+
+    if not extracted_graph:
+        return
+
+    # Run the coloring algorithm on the meta graph.
+    node_channels = allocation_algorithms.coloring_algorithm(extracted_graph['graph'], extracted_graph['known_nodes'])
+
+    # Compare the optimal channel with the actual channel of every node.
+    for node in node_channels:
+        if node['optimal_channel'] != node['current_channel']:
+            # Issue a warning for that node.
+            pass
