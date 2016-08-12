59bd96c5c3b054b9cf3bfcfad355b4a186f27a01
masha-len-ub-g-i3 <david.marn@gmail.com>
2016-08-12 14:58:49 -0700

    highest_2ghz_channel no longer hard-coded.

--- nodewatcher/modules/analysis/channel_allocation/channel_lookup.py
+++ nodewatcher/modules/analysis/channel_allocation/channel_lookup.py
@@ -31,10 +31,10 @@ def get_available_channels(node_bssid):
     :return: Array of available channels.
     """
     # TODO: Turn hard-coded array into a node-specific (stemming from regulatory practices) property.
-    highest_2ghz_channel = 11
     ch_2ghz = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     ch_5ghz = [36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 100, 102, 104, 106, 108, 110, 112, 114, 116,
                118, 120, 122, 124, 126, 128, 132, 134, 136, 138, 140, 142, 144, 149, 151, 153, 155, 157, 159, 161, 165]
+    highest_2ghz_channel = max(ch_2ghz)
 
     if models.WifiInterfaceMonitor.objects.filter(bssid=node_bssid)[0].channel <= highest_2ghz_channel:
         return ch_2ghz
