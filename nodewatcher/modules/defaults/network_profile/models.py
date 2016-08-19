b3334af72874a336bb09022fc9b9e23650e33323
Jernej Kos <jernej@kos.mx>
2016-08-19 14:32:11 +0200

    network_profile: Add "use hostname as ESSID" profile.

--- nodewatcher/modules/defaults/network_profile/models.py
+++ nodewatcher/modules/defaults/network_profile/models.py
@@ -36,4 +36,5 @@ registration.point('node.config').register_choice('network.profile#profiles', re
 registration.point('node.config').register_choice('network.profile#profiles', registration.Choice('wifi-uplink', _("Use a wireless interface for uplink")))
 registration.point('node.config').register_choice('network.profile#profiles', registration.Choice('no-wifi-ap', _("Disable wireless AP")))
 registration.point('node.config').register_choice('network.profile#profiles', registration.Choice('no-wifi-mesh', _("Disable wireless mesh")))
+registration.point('node.config').register_choice('network.profile#profiles', registration.Choice('hostname-essid', _("Use hostname as ESSID")))
 registration.point('node.config').register_item(NetworkProfileConfig)
