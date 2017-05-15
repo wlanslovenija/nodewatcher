69a8c3d1bf4825cdbd35d95028ed70a7c5c38fb8
Robert Marko <robimarko@gmail.com>
2017-05-16 00:03:24 +0200

    Add Edgerouter X device descriptor Include Edgerouter device descriptors Port configuration fixed Select correct LEDE device profile
    Correct LEDE device profile selected for making

--- nodewatcher/modules/devices/ubnt/__init__.py
+++ nodewatcher/modules/devices/ubnt/__init__.py
@@ -1,2 +1,2 @@
 # UBNT device modules.
-from . import nano, bullet, rocket, pico, powerbeam
+from . import nano, bullet, rocket, pico, powerbeam, edgerouter
---
+++ nodewatcher/modules/devices/ubnt/edgerouter.py
@@ -0,0 +1,59 @@
+from django.utils.translation import ugettext_lazy as _
+
+from nodewatcher.core.generator.cgm import base as cgm_base, protocols as cgm_protocols, devices as cgm_devices
+
+
+class UBNTEdgerouterX(cgm_devices.DeviceBase):
+    """
+    UBNT EdgerouterX device descriptor.
+    """
+
+    identifier = 'ub-edgerouter-x'
+    name = "Edgerouter X"
+    manufacturer = "Ubiquiti"
+    url = 'http://www.ubnt.com/'
+    architecture = 'ramips_mt7621'
+    radios = []
+    switches = [
+        cgm_devices.Switch(
+            'sw0', "Gigabit switch",
+            ports=[0, 1, 2, 3, 4, 6],
+            cpu_port=6,
+            cpu_tagged=True,
+            vlans=16,
+            configurable=True,
+            presets=[
+                cgm_devices.SwitchPreset('default', _("Default VLAN configuration"), vlans=[
+                    cgm_devices.SwitchVLANPreset(
+                        'wan0', "Wan0",
+                        vlan=2,
+                        ports=[0, 6],
+                    ),
+                    cgm_devices.SwitchVLANPreset(
+                        'lan0', "Lan0",
+                        vlan=1,
+                        ports=[1, 2, 3, 4, 6],
+                    ),
+                ])
+            ]
+        ),
+    ]
+    ports = []
+    antennas = []
+    port_map = {
+        'openwrt': {
+            'sw0': cgm_devices.SwitchPortMap('switch0', vlans='eth0.{vlan}'),
+        }
+    }
+    profiles = {
+        'lede': {
+            'name': 'ubnt-erx',
+            'files': [
+                '*-mt7621-generic-ubnt-erx-squashfs-factory.bin',
+                '*-mt7621-generic-ubnt-erx-squashfs-sysupgrade.bin',
+            ]
+        }
+    }
+
+# Register Edgerouter X device.
+cgm_base.register_device('lede', UBNTEdgerouterX)
