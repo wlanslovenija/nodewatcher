ed578df4c0979a872413348ba41307e12c3c8e35
Raslav Milutinović <raslav.milutinovic@gmail.com>
2017-06-28 06:28:17 +0200

    Added topnode calculation, zooming to topnodes, drawing actual api nodes to svg, loading nodes asyncronusly and drawaing them to svg.

--- nodewatcher/modules/frontend/ip_space/static/js/DrawCidrs.js
+++ nodewatcher/modules/frontend/ip_space/static/js/DrawCidrs.js
@@ -8,6 +8,7 @@ window.DrawCidr = class DrawCidr{
 		this.ips_pixel = this.ips_side/this.size;
 		this.offset = this.ip2num(this.subnet.split("/")[0]);
 		this.draw(subnet);
+		this.data = new Array();
 	}
 	ip2num(ip){
                 var d = ip.split('.');
@@ -23,13 +24,15 @@ window.DrawCidr = class DrawCidr{
                 return d;
 	}
 	subnetSize(subnet){
-		var times = 32-parseInt(subnet.split("/")[1]);
+	    var subnet_str = String(subnet);
+		var times = 32-parseInt(subnet_str.split("/")[1]);
 		var subnet_ips = Math.pow(2, times);
 		var shape_size = Math.sqrt(subnet_ips)/this.ips_pixel;
 		return shape_size;
 	}
 	subnetXY(subnet){
-		var start_num = this.ip2num(subnet.split("/")[0])-this.offset;
+		var subnet_str = String(subnet);
+		var start_num = this.ip2num(subnet_str.split("/")[0])-this.offset;
 		var start_xy = d2xy(start_num);
 
 		var x=start_xy.x/this.ips_pixel;
@@ -64,7 +67,6 @@ window.DrawCidr = class DrawCidr{
 	    var hex = c.toString(16);
 	    return hex.length == 1 ? "0" + hex : hex;
 	}
-
 	rgbToHex(r, g, b) {
 	    return "#" + this.componentToHex(r) + this.componentToHex(g) + this.componentToHex(b);
 	}
@@ -80,6 +82,51 @@ window.DrawCidr = class DrawCidr{
 		var x=start_xy.x/this.ips_pixel;
 		var y=start_xy.y/this.ips_pixel;
 
-		this.svg.append("rect").attr("x", x).attr("y", y).attr("height", shape_size).attr("width", shape_size).style("fill", this.rgbToHex(times*8,times*8,times*8)).attr("id", subnet).attr("n", subnet_ips).attr("d", description);
+		this.svg.append("rect").attr("x", x).attr("y", y).attr("height", shape_size).attr("width", shape_size).style("fill", this.rgbToHex(times*8,times*8,times*8)).style("opacity", 0.3).attr("id", subnet).attr("n", subnet_ips).attr("d", description);
+	}
+	load(){
+	    var self = this;
+        for(var i=0; i<33; i++){
+            $.getJSON( "https://nodes.wlan-si.net/api/v2/pool/ip/?format=json&prefix_length="+i, function( data) {
+                for(var j=0; j<data.results.length; j++){
+                    self.draw(data.results[j].network+"/"+data.results[j].prefix_length, data.results[j].description);
+                }
+                self.data.push(data);
+                if(self.data.length==33){
+                    jQuery('rect').tipsy({
+                        gravity: 'w',
+                        html: true,
+                        title: function() {
+                            return this.id+"<br>Number of hosts: "+$(this).attr("n")+"<br>Network name: "+$(this).attr("d");
+                        }
+                    });
+                    self.setTopNodes(self.calcTopNodes(self.data));
+                }
+            });
+        }
+	}
+	calcTopNodes(data){
+	    var res = new Array();
+	    for(var i=0;i<data.length;i++){
+	        for(var j=0;j<data[i].results.length;j++){
+	            var node = data[i].results[j];
+                if(node["@id"] == node.top_level["@id"]){
+                    res.push(node);
+                }
+	        }
+	    }
+	    return res;
+	}
+	setTopNodes(data){
+		var self = this;
+        $("#topnodes").html("<li id='cidr'><a href='#' value='10.0.0.0/8'>10.0.0.0/8 (Zoom out)</a></li>");
+        data.forEach(function(node) {
+            $("#topnodes").append("<li id='cidr'><a href='#' cidr='"+node.network+"/"+node.prefix_length+"'>"+node.network+"/"+node.prefix_length+" ("+node.description+")"+"</option>");
+        });
+        $("#topnodes").on("click", function(event){
+            var scale= self.size/self.subnetSize($(event.target).attr("cidr"));
+            var start = self.subnetXY($(event.target).attr("cidr"));
+            self.svg.selectAll("rect").attr('transform', 'translate(' + start[0]*-1*scale + ',' + start[1]*-1*scale + ') scale(' + scale + ')');
+        });
 	}
 }
