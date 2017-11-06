9fe27f5527b4296e0ff5551f104bccdaaadd7d76
Robert Marko <robimarko@gmail.com>
2017-11-06 11:53:42 +0100

    devices: Add Mikrotik SXT Lite 2

--- nodewatcher/modules/devices/mikrotik/sxt.py
+++ nodewatcher/modules/devices/mikrotik/sxt.py
@@ -8,7 +8,7 @@ class MikrotikRBSXT5nDr2(cgm_devices.DeviceBase):
     Mikrotik RB SXT5 nDr2 (SXT Lite 5) device descriptor.
     """
 
-    identifier = 'mt-rbsxt-ndr2'
+    identifier = 'mt-rbsxt5-ndr2'
     name = "RBSXT5nDr2 (SXT Lite 5)"
     manufacturer = "MikroTik"
     url = 'https://mikrotik.com/product/RBSXT5nDr2'
@@ -62,5 +62,30 @@ class MikrotikRBSXT5nDr2(cgm_devices.DeviceBase):
         }
     }
 
+
+class MikrotikRBSXT2nDr2(MikrotikRBSXT5nDr2):
+    """
+    Mikrotik RB SXT2 nDr2 (SXT Lite 2) device descriptor.
+    """
+
+    identifier = 'mt-rbsxt2-ndr2'
+    name = "RBSXT2nDr2 (SXT Lite 2)"
+    url = 'https://mikrotik.com/product/RBSXT2nDr2'
+    radios = [
+        cgm_devices.IntegratedRadio('wifi0', _("Integrated wireless radio"), [
+            cgm_protocols.IEEE80211BGN(
+                cgm_protocols.IEEE80211BGN.SHORT_GI_20,
+                cgm_protocols.IEEE80211BGN.SHORT_GI_40,
+                cgm_protocols.IEEE80211BGN.RX_STBC1,
+                cgm_protocols.IEEE80211BGN.DSSS_CCK_40,
+            )
+        ], [
+            cgm_devices.AntennaConnector('a1', "Antenna0")
+        ], [
+            cgm_devices.DeviceRadio.MultipleSSID,
+        ])
+    ]
+
 # Register the Mikrotik SXT devices.
 cgm_base.register_device('lede', MikrotikRBSXT5nDr2)
+cgm_base.register_device('lede', MikrotikRBSXT2nDr2)
