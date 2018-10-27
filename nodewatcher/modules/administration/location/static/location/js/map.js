153ec7c138cd37ab2644b5044bb556c22207a59e
Marin Stević <marin.stevic@gmail.com>
2018-10-27 20:27:28 +0200

    GSoC: Updating the map
    This is an update to the current nodewatcher map. I have added:
    
    -Fulscreen option
    -Popup with some general information about the node that you get when you click on it. And also by clicking the name in the popup you can go to that nodes page
    -Sidebar that gives you a list of all currently online nodes with a search bar and the ability to locate each node on the map
    -Sidebar also shows all nodes that have gone offline in the past 24 hours
    -Updated the markercluster.js to work with the new leaflet.js
    -Leaflet markercluster and fullscreen are now gathered through CDN so they will be the newest version

--- nodewatcher/modules/administration/location/static/location/js/map.js
+++ nodewatcher/modules/administration/location/static/location/js/map.js
@@ -5,8 +5,10 @@
 
     // Create a marker for each location
     $.nodewatcher.map.extendNode(function(node) {
-        if (node.data.l)
-            return L.marker({lng: node.data.l[0], lat: node.data.l[1]});
+        if (node.data.l) {
+            sidebarTableAddNode(node,"node-list-table");  //Add the node to the sidebar active node list
+            return L.marker({lng: node.data.l[0], lat: node.data.l[1]}).bindPopup('Name: <a href="../node/' + node.data.i + '">' + node.data.n + '</a><br>Type: ' + node.data.t);  //Show a popup when a node is selected with its name and type
+        }
     });
 
     // Create a polyline for each link
